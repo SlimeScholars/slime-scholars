16f39ef0e70f80463167dfae688318c9c704feb8
jiayishen21 <lucasshen21@gmail.com>
2023-07-14 16:30:37 +0800

    debugged friend api

--- pages/api/user/friend/delete-received.js
+++ pages/api/user/friend/delete-received.js
@@ -5,10 +5,10 @@ import connectDB from '../../../../utils/connectDB'
 import User from '../../../../models/userModel'
 
 /**
- * @desc    Remove friend
- * @route   POST /api/user/remove-friend
+ * @desc    Delete received friend request
+ * @route   POST /api/user/friend/delete-received
  * @access  Private
- * @param   {string} req.body.friendId - Account id of friend you want delete
+ * @param   {string} req.body.friendId - Account id of person you want delete the request of
  */
 export default async function (req, res) {
   try {
@@ -24,33 +24,33 @@ export default async function (req, res) {
 
     // Make sure user is a student
     checkUserType(user, 1)
-    
+
     const { friendId } = req.body
 
-    const friendIdObj = mongoose.Types.ObjectId(friendId)
+    const friendIdObj = new mongoose.Types.ObjectId(friendId)
 
-    if(!user.friends.includes(friendIdObj)) {
-      throw new Error('You already were not friends with anyone of that id')
+    if(!user.receivedFriendRequests.includes(friendIdObj)) {
+      throw new Error('You did not send a friend request of that id')
     }
 
     const friend = await User.findById(friendIdObj)
-
-    // This shouldn't happen usually, but if a user gets deleted, it could happen
     if(!friend) {
-      throw new Error('Could not find that friend')
+      throw new Error('Cannot find user of that id')
     }
 
-    user.friends = user.friends.filter((element) => element == friendIdObj)
-    friend.friends = friend.friends.filter((element) => element == user._id)
+    // Delete friend request
+    user.receivedFriendRequests = user.friends.filter((element) => element == friendIdObj)
+    friend.sentFriendRequests = friend.friends.filter((element) => element == user._id)
 
+    // Update database
     await User.findByIdAndUpdate(user._id, {
-      friends: user.friends,
+      receivedFriendRequests: user.receivedFriendRequests,
     })
     await User.findByIdAndUpdate(friendIdObj, {
-      friends: friend.friends,
+      sentFriendRequests: friend.sentFriendRequests,
     })
 
-    res.status(200).json({friends: user.friends})
+    res.status(200).json({receivedFriendRequests: user.receivedFriendRequests})
 
   } catch(error) {
     res.status(400).json({message: error.message})
