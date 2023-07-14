3b00bb90fc3ced4ff4de3e28f1a55e80f6d34367
jiayishen21 <lucasshen21@gmail.com>
2023-07-14 11:29:06 +0800

    api routes for login and update, authentication util

---
+++ utils/authenticate.js
@@ -0,0 +1,41 @@
+import connectDB from './connectDB'
+const jwt = require('jsonwebtoken')
+import User from '../models/userModel'
+
+export const authenticate = async(authorization) => {
+  // Connect to database
+  await connectDB()
+
+  // authorization is req.headers.authorization
+  let token
+  
+  if (
+    authorization &&
+    authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Take out the 'Bearer' part
+      token = authorization.split(' ')[1]
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+      // Get user from the token
+      const user = await User.findById(decoded.id)
+      if(user) {
+        user.password = undefined
+        return user
+      }
+      else {
+        throw new Error('User not found')
+      }
+      
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
+
+  if (!token) {
+    throw new Error('Not authorized, no token')
+  }
+}
