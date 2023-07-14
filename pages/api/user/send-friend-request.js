27a78a16f1771a19f06734ab164f42f25b85b1c5
jiayishen21 <lucasshen21@gmail.com>
2023-07-14 16:02:43 +0800

    created util checkUserType and added it to the student only routes

--- pages/api/user/send-friend-request.js
+++ pages/api/user/send-friend-request.js
@@ -1,5 +1,6 @@
 import { mongoose } from 'mongoose'
 import { authenticate } from "../../../utils/authenticate"
+import { checkUserType } from '../../../utils/checkUserType'
 import connectDB from '../../../utils/connectDB'
 import User from '../../../models/userModel'
 
@@ -20,6 +21,9 @@ export default async function (req, res) {
 
     // Authenticate and get user
     const user = await authenticate(req.headers.authorization)
+
+    // Make sure user is a student
+    checkUserType(user, 1)
     
     const { friendId } = req.body
 
