887ce212815ddc384e24938e0a47ef372289dec5
jiayishen21 <lucasshen21@gmail.com>
2023-07-14 15:41:47 +0800

    api route remove-friend

---
+++ pages/api/user/remove-friend.js
@@ -0,0 +1,54 @@
+import { mongoose } from 'mongoose'
+import { authenticate } from "../../../utils/authenticate"
+import connectDB from '../../../utils/connectDB'
+import User from '../../../models/userModel'
+
+/**
+ * @desc    Remove friend
+ * @route   POST /api/user/remove-friend
+ * @access  Private
+ * @param   {string} req.body.friendId - Account id of friend you want delete
+ */
+export default async function (req, res) {
+  try {
+    if(req.method !== 'POST') {
+      throw new Error(`${req.method} is an invalid request method`)
+    }
+
+    // Connect to database
+    await connectDB()
+
+    // Authenticate and get user
+    const user = await authenticate(req.headers.authorization)
+    
+    const { friendId } = req.body
+
+    const friendIdObj = mongoose.Types.ObjectId(friendId)
+
+    if(!user.friends.includes(friendIdObj)) {
+      throw new Error('You already were not friends with anyone of that id')
+    }
+
+    const friend = await User.findById(friendIdObj)
+
+    // This shouldn't happen usually, but if a user gets deleted, it could happen
+    if(!friend) {
+      throw new Error('Could not find that friend')
+    }
+
+    user.friends = user.friends.filter((element) => element == friendIdObj)
+    friend.friends = friend.friends.filter((element) => element == user._id)
+
+    await User.findByIdAndUpdate(user._id, {
+      friends: user.friends,
+    })
+    await User.findByIdAndUpdate(friendIdObj, {
+      friends: friend.friends,
+    })
+
+    res.status(200).json({friends: user.friends})
+
+  } catch(error) {
+    res.status(400).json({message: error.message})
+  }
+}
