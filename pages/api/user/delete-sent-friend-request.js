041a7397ec16a5471c0a34ab5c51769f8b7774f3
jiayishen21 <lucasshen21@gmail.com>
2023-07-14 15:47:32 +0800

    api route delete-sent-friend-request

---
+++ pages/api/user/delete-sent-friend-request.js
@@ -0,0 +1,54 @@
+import { mongoose } from 'mongoose'
+import { authenticate } from "../../../utils/authenticate"
+import connectDB from '../../../utils/connectDB'
+import User from '../../../models/userModel'
+
+/**
+ * @desc    Delete sent request
+ * @route   POST /api/user/delete-sent-request
+ * @access  Private
+ * @param   {string} req.body.friendId - Account id of person you want delete the request of
+ */
+export default async function (req, res) {
+  try {
+    if(req.method !== 'POST') {
+      throw new Error(`${req.method} is an invalid request method`)
+    }
+
+    // Connect to database
+    await connectDB()
+
+    // Authenticate and get user
+    const user = await authenticate(req.headers.authorization)
+
+    const { friendId } = req.body
+
+    const friendIdObj = mongoose.Types.ObjectId(friendId)
+
+    if(!user.sentFriendRequests.includes(friendIdObj)) {
+      throw new Error('You did not send a friend request of that id')
+    }
+
+    const friend = await User.findById(friendIdObj)
+    if(!friend) {
+      throw new Error('Cannot find user of that id')
+    }
+
+    // Delete friend request
+    user.sentFriendRequests = user.friends.filter((element) => element == friendIdObj)
+    friend.receivedFriendRequests = friend.friends.filter((element) => element == user._id)
+
+    // Update database
+    await User.findByIdAndUpdate(user._id, {
+      sentFriendRequests: user.sentFriendRequests,
+    })
+    await User.findByIdAndUpdate(friendIdObj, {
+      receivedFriendRequests: friend.receivedFriendRequests,
+    })
+
+    res.status(200).json({friends: user.sentFriendRequests})
+
+  } catch(error) {
+    res.status(400).json({message: error.message})
+  }
+}
