a1056e372063b6b2cea0129a03def2600d8c9302
jiayishen21 <lucasshen21@gmail.com>
2023-07-15 14:54:22 +0800

    update how classes work. instead of each teacher having their own class code, teachers can create classes, which have their own class codes. students can join multiple classes and teachers can run multiple classes.

--- data/userData.js
+++ data/userData.js
@@ -4,6 +4,8 @@ export const userData = {
 	maxPasswordLength: 55,
 	minUsernameLength: 2,
 	maxUsernameLength: 15,
+	minClassNameLength: 2,
+	maxClassNameLength: 60,
 	maxEmailLength: 255,
 	maxNameLength: 55,
 	honorifics: ["Mr.", "Ms.", "Mrs.", "Dr.", "Jr."],
---
+++ pages/api/class/create.js
@@ -0,0 +1,65 @@
+import { verifyClassName } from "../../../utils/verify"
+import { generateClassCode } from "../../../utils/generateClassCode"
+import { authenticate } from "../../../utils/authenticate"
+import { userData } from "../../../data/userData"
+import { checkUserType } from '../../../utils/checkUserType'
+import connectDB from '../../../utils/connectDB'
+import User from '../../../models/userModel'
+import Class from '../../../models/classModel'
+
+/**
+ * @desc    Update user's account information, but not password
+ * @route   POST /api/class/create
+ * @access  Private
+ * @param   {string} req.body.className - Max 60 characters long.
+ */
+
+export default async function (req, res) {
+  try {
+    if(req.method !== 'POST') {
+      throw new Error(`${req.method} is an invalid request method`)
+    }
+
+    // Connect to database
+    await connectDB()
+
+    // Authenticate and get user
+    const user = await authenticate(req.headers.authorization)
+
+    // Make sure user is a teacher
+    checkUserType(user, 3)
+
+    const { className } = req.body
+
+    verifyClassName(className)        
+
+    let classCode = generateClassCode(userData.classCodeLength)
+    let counter = 0
+    let classExists = await Class.findOne({classCode})
+    while(classExists && counter < 10) {
+      classCode = generateClassCode(userData.classCodeLength)
+      classExists = await Class.findOne({classCode})
+    }
+
+    const newClass = await Class.create({
+      className,
+      classCode,
+      teacherIds: [user._id],
+      studentIds: [],
+    })
+
+    user.classes.push(newClass._id)
+    await User.findByIdAndUpdate(user._id, {
+      classes: user.classes
+    })
+    const newUser = await User.findById(user._id)
+
+    res.status(201).json({
+      class: newClass,
+      user: newUser,
+    })
+
+  } catch(error) {
+    res.status(400).json({message: error.message})
+  }
+}
