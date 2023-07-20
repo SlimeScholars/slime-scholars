54de3df718aa565ebb509985b65bc90036ecb312
jiayishen21 <lucasshen21@gmail.com>
2023-07-20 13:20:58 +0800

    moved spinner to own component and made loading not force a remount of component

---
+++ components/spinner.jsx
@@ -0,0 +1,22 @@
+import PuffLoader from "react-spinners/PuffLoader";
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+};
+
+export default function Spinner() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <PuffLoader
+        color={"#F649DA"}
+        loading={true}
+        css={override}
+        size={150}
+      />
+      <h1 className="text-5xl font-black text-primary mt-10 phase">
+        Loading...
+      </h1>
+    </div>
+  )
+}
