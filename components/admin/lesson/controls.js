69db3e657da2bab423f3e4e712696a03983fcb1f
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-07-26 19:17:47 -0400

    add text section

--- components/admin/lesson/controls.js
+++ components/admin/lesson/controls.js
@@ -8,27 +8,28 @@ export default function SectionControls({
   moveSection,
 }) {
   return (
-    <div>
+    <div className="text-bg-light">
       <input
-        className="bg-purple-500 flex items-center justify-center absolute -left-12 w-12 h-12"
+        className="bg-purple-500/80 hover:bg-purple-400/80 font-averia font-bold text-xl flex text-center items-center justify-center absolute -left-12 w-12 h-12"
         onChange={(e) => changeSectionNumber(section.index, e.target.value)}
         value={section.sectionNumber}
+        type="number"
       />
       <button
-        className="bg-pink-500 flex items-center justify-center absolute -left-24 w-12 h-12"
+        className="bg-pink-500/80 hover:bg-pink-400/80 flex items-center justify-center absolute -left-24 w-12 h-12"
         onClick={() => deleteSection(section.index)}
       >
         <FaTrashAlt />
       </button>
-      <div className="bg-green-400 flex flex-col absolute -left-36 w-12 h-12">
+      <div className="flex flex-col absolute -left-36 w-12 h-12">
         <button
-          className="w-full bg-green-400 h-1/2 flex items-center justify-center"
+          className="w-full bg-green-400/80 hover:bg-green-300/80 h-1/2 flex items-center justify-center"
           onClick={() => moveSection(section.index, -1)}
         >
           <FaChevronUp />
         </button>
         <button
-          className="w-full bg-blue-400 h-1/2 flex items-center justify-center"
+          className="w-full bg-blue-400/80 hover:bg-blue-300/80 h-1/2 flex items-center justify-center"
           onClick={() => moveSection(section.index, 1)}
         >
           <FaChevronDown />
