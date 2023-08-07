766113ba720c45efb99a5f3526b3f1e008150c1b
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-08-07 18:16:37 -0400

    lesson display

--- components/admin/lesson/sections/img.js
+++ components/admin/lesson/sections/img.js
@@ -8,32 +8,34 @@ export default function ImgSection({
   changeSectionNumber,
   deleteSection,
   moveSection,
+  active,
 }) {
   const [img, setImg] = useState(null);
   useEffect(() => {
     if (image) {
       try {
-        if(typeof image === 'string') {
-          setImg(image)
-        }
-        else {
+        if (typeof image === "string") {
+          setImg(image);
+        } else {
           let url = URL.createObjectURL(image);
           setImg(url);
         }
       } catch (error) {
-        showToastMessage(error.message)  
+        showToastMessage(error.message);
       }
     }
   }, [image]);
 
   return (
     <div className="w-full relative py-3 px-6 flex flex-col justify-start items-start bg-purple-50">
-      <Controls
-        section={section}
-        changeSectionNumber={changeSectionNumber}
-        deleteSection={deleteSection}
-        moveSection={moveSection}
-      />
+      {!active && (
+        <Controls
+          section={section}
+          changeSectionNumber={changeSectionNumber}
+          deleteSection={deleteSection}
+          moveSection={moveSection}
+        />
+      )}
       <div className="w-full flex justify-center items-center mt-5">
         {img && (
           <img src={img} alt="img" className="w-full h-full object-contain" />
--- utils/verify.js
+++ utils/verify.js
@@ -109,7 +109,27 @@ export const verifyClassName = (className) => {
   }
 };
 
-export const showToastMessage = (text) => {
+export const showToastMessage = (text, success) => {
+  if (success) {
+    toast.success(text, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: 5000,
+      style: {
+        backgroundColor: "#689e5790",
+        fontFamily: "Galindo",
+        color: "#FEF8FF",
+        letterSpacing: "0.5px",
+        lineHeight: "1.4",
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+        paddingTop: "0.5rem",
+        paddingBottom: "0.5rem",
+      },
+      className: "text-sm",
+    });
+    return;
+  }
+
   toast.error(text, {
     position: toast.POSITION.BOTTOM_RIGHT,
     autoClose: 5000,
