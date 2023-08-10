a0db5546a2d17083ab85449acde1f8d734b1ff52
jiayishen21 <lucasshen21@gmail.com>
2023-08-10 23:09:13 +0800

    get courses with completion status

--- pages/api/learn/courses.js
+++ pages/api/learn/courses.js
@@ -24,17 +24,32 @@ export default async function (req, res) {
     checkUserType(user, 1)
 
     const courses = await Course.find({})
+      .select('_id courseName units')
       .populate({
         path: 'units',
-        select: '_id courseName',
+        select: '_id unitName',
       })
     
+    const modifiedCourses = []
     // Check user for completed
-    for(let i in user.completedCourses) {
-      console.log(i)
+    for(let i in courses) {
+      modifiedCourses.push({
+        _id: courses[i]._id,
+        courseName: courses[i].courseName,
+        units: courses[i].units,
+        tier: 0,
+      })
+      for(let j in user.completedCourses) {
+        if(
+          (user.completedCourses[j].course._id && user.completedCourses[j]._id.equals(courses[i]._id)) ||
+          user.completedCourses[j].course.equals(courses[i]._id)
+        ) {
+          modifiedCourses[i].tier = user.completedCourses[j].tier
+        }
+      }
     }
 
-    res.json({ courses: courses })
+    res.json({ courses: modifiedCourses })
   } catch(error) {
     res.status(400).json({message: error.message})
   }
