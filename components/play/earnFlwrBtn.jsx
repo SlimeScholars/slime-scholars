f9fd2fe34772fbac9cd162f925e6bace29f34449
huang9185 <helyn20012@outlook.com>
2023-08-13 15:52:11 -0400

    add button images and react functions

---
+++ components/play/earnFlwrBtn.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+/*
+Button to earn flowers
+
+Bg Color:
+    Red: 228
+    Green: 117
+    Blue: 136
+*/
+
+export default function earnFlwrBtn() {
+
+    return (
+        <button className={
+            "pink-bg sqr-btn lg-btn"
+        }>
+            
+        </button>
+    )
+}
