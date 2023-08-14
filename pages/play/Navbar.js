7c57e01a675084673e7c3ce8547c6abfac2a8060
huang9185 <helyn20012@outlook.com>
2023-08-14 14:32:13 -0400

    Earn flowers and buttons visual done

--- pages/play/Navbar.js
+++ pages/play/Navbar.js
@@ -2,11 +2,19 @@ import React from "react";
 import EarnFlwrBtn from '../../components/play/earnFlwrBtn';
 
 const Navbar = () => {
+
+  const types = [
+    { title: 'shopping', id: 1},
+    { title: 'friends', id: 2},
+    { title: 'slimes', id: 3},
+    { title: 'backpack', id: 4}
+  ]
+
   return (
     <div className="flex flex-row items-center justify-between p-5">
       {/* earn flowers button */}
-      <div className="p-8 bg-red-300">
-        <button>Earn Flowers</button>
+      <div className="p-8 bg-red-300 hover:bg-red-300/75">
+        <EarnFlwrBtn />
       </div>
       <div className="flex flex-row space-x-2">
         <div className="flex flex-col justify-end">
@@ -14,10 +22,19 @@ const Navbar = () => {
           <div className="bg-[#5A5A5A] opacity-60 h-8 w-12 rounded-md mt-1"></div>
         </div>
         {/* buttons and icons */}
-        <button className="p-12 bg-red-300 rounded-full "></button>
-        <button className="p-12 bg-red-300 rounded-full "></button>
-        <button className="p-12 bg-red-300 rounded-full "></button>
-        <button className="p-12 bg-red-300 rounded-full "></button>
+        {
+          types.map(type => {
+            const imgLink = "/assets/icons/"+type.title+".png"
+            return (
+                <button 
+                    onClick="../courses/index"
+                    className="p-8 bg-red-300 rounded-full hover:bg-red-300/75">
+                    <img src={imgLink} className="h-14 w-14">
+                    </img>
+                </button>
+            );
+          })
+        }
       </div>
     </div>
   );
