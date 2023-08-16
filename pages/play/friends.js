5d719138390e02f494cdc224656e7a5520040c3c
huang9185 <helyn20012@outlook.com>
2023-08-16 14:11:39 -0400

    sync

--- pages/play/friends.js
+++ pages/play/friends.js
@@ -6,17 +6,6 @@ import axios from "axios";
 export default function Friends({ loading, user }) {
     const router = useRouter();
 
-    axios.post('/slimes', {
-        
-    }
-    )
-    .then((response)=>{
-        console.log(response)
-    })
-    .catch((error) => {
-        console.error(error.message);
-    })
-
     useEffect(() => {
         if (loading) {return;}
         if (!user || user.userType !== 1) {
@@ -25,27 +14,46 @@ export default function Friends({ loading, user }) {
     }, [user, loading]);
 
     return (
-        <div className="w-screen h-screen bg-cover bg-[url('/assets/backgrounds/bg-beach.png')]">
+        <div className="p-8 w-screen h-screen bg-cover bg-[url('/assets/backgrounds/bg-beach.png')]">
             <Navbar
                 current="2"
             ></Navbar>
 
-            <div className="p-8 items-center justify-between p-5">
-                {/*  Add Friend */}
-                <div className="flex flex-row bg-white/75 rounded-lg items-center">
-                    <div className="grow-0 pl-4">
-                        <img src="/assets/icons/friends.png" className="h-20 w-20"></img>
-                    </div>
-                    <div  className="grow pl-4 font-galindo text-xl">
-                        Friends
-                    </div>
-                    <div className="grow-0 flex grow pr-4">
-                        <button className="p-2 text-xl bg-red-300 hover:bg-red-300/50 rounded-lg font-galindo">
-                            Add Friends
-                        </button>
+            <div className="pt-5">
+                <div className="items-center justify-between">
+                    {/*  Add Friend  and others (TODO) */}
+                    <div className="flex flex-row bg-white/75 rounded-lg items-center">
+                        <div className="grow-0 pl-4">
+                            <img src="/assets/icons/friends.png" className="h-20 w-20"></img>
+                        </div>
+                        <div  className="grow pl-4 font-galindo text-xl">
+                            Friends
+                        </div>
+                        <div className="grow-0 flex grow pr-4">
+                            <button className="p-2 text-xl bg-red-300 hover:bg-red-300/50 rounded-lg font-galindo"
+                                onClick={() => {
+                                    const token = localStorage.getItem('jwt');
+                                    const config = {
+                                        headers: {
+                                        Authorization: `Bearer ${token}`,
+                                        },
+                                    };
+                                    axios.post('/api/slime/level-up', {
+                                    }, config)
+                                    .then((response)=>{
+                                        console.log(response)
+                                    })
+                                    .catch((error) => {
+                                        console.error(error.message);
+                                    })
+                                }}>
+                                Add Friends
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+            
         </div>
     );
 }
