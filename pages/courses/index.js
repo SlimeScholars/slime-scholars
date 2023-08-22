7fb1df5b66d374812196836c57c56a4efd34e4bb
jiayishen21 <lucasshen21@gmail.com>
2023-08-22 15:38:20 -0400

    delete random word document in icons folder

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,6 +1,22 @@
-export default function Home({ user, setUser }) {
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+export default function Home({ user, loading, }) {
+	const router = useRouter()
+
+	useEffect(() => {
+		if (loading) {
+			return
+		}
+		if (!user || user.userType !== 1) {
+			router.push('/')
+		}
+	}, [user, loading])
+
 	return (
-		<div>lol</div>
+		<div>lol
+
+		</div>
 	)
 }
 
