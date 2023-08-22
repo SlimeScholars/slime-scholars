786f8ef3d3779839a203111817605ad91afddf17
Alex Xu <alex.xu.205@gmail.com>
2023-08-22 15:27:42 -0400

    dynamic bg change added to all play pages

--- pages/play/shopping.js
+++ pages/play/shopping.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { Navbar } from "../../components/play/Navbar";
+import { gameData } from "../../data/gameData";
 
 export default function Shopping({ loading, user }) {
   const router = useRouter();
+  const [bg, setBg] = useState("bg-beach.png"); // Default background
 
   useEffect(() => {
     if (loading) {
@@ -11,15 +13,20 @@ export default function Shopping({ loading, user }) {
     }
     if (!user || user.userType !== 1) {
       router.push("/");
+    } else {
+      if (user.bg && gameData.items[user.bg].bg) {
+        setBg(gameData.items[user.bg].bg);
+      }
     }
   }, [user, loading]);
+  console.log(bg);
 
   return (
-    <div className="w-screen h-screen bg-cover bg-[url('/assets/backgrounds/bg-beach.png')]">
-      <div className="w-screen h-screen bg-cover bg-[url('/assets/backgrounds/bg-beach.png')] ">
-        <div className="p-8 w-full h-full justify-center items-center backdrop-brightness-50">
-          <Navbar current="1" className=""></Navbar>
-        </div>
+    <div
+      className={`w-screen h-screen bg-cover bg-[url('/assets/backgrounds/${bg}')]`}
+    >
+      <div className="p-8 w-full h-full justify-center items-center backdrop-brightness-50">
+        <Navbar current="1" className=""></Navbar>
       </div>
     </div>
   );
