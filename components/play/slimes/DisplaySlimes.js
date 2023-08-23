a120d6f647d623f80fc444f592d5bdc6baba6adb
Alex Xu <alex.xu.205@gmail.com>
2023-08-23 13:42:00 -0400

    level up from homepage

--- components/play/slimes/DisplaySlimes.js
+++ components/play/slimes/DisplaySlimes.js
@@ -3,12 +3,12 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { showToastError } from "../../../utils/toast";
 
-export default function DisplaySlimes({ user, setLoading }) {
+export default function DisplaySlimes({ user, setLoading, setUser }) {
   if (!user) return <></>;
 
   const router = useRouter();
 
-  //   handle click should automatically level up the slime given the user has enough slime gel
+  //   handle click should automatically level up the slime and update the user
   const handleClick = (id) => {
     console.log(id);
     try {
@@ -24,7 +24,14 @@ export default function DisplaySlimes({ user, setLoading }) {
       axios
         .post("/api/slime/level-up", { slimeId: id }, config)
         .then((response) => {
-          console.log(response.data.message);
+          console.log(response.data);
+          const newUser = {
+            ...user,
+            roster: response.data.roster,
+            slimeGel: response.data.slimeGel,
+            slimes: response.data.slimes,
+          };
+          setUser(newUser);
           setLoading(false);
         })
         .catch((error) => {
@@ -49,8 +56,9 @@ export default function DisplaySlimes({ user, setLoading }) {
             return (
               <div
                 key={index}
-                className={`flex flex-col ${offset ? "transform -translate-y-16" : ""
-                  }`}
+                className={`flex flex-col ${
+                  offset ? "transform -translate-y-16" : ""
+                }`}
               >
                 <div className="flex flex-row items-center mx-auto">
                   <div className="bg-[#5A5A5A] opacity-60 h-5 w-28 pb-6 rounded-md mx-auto text-white text-center">
