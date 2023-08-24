10076ec56afc29d00bc933de7a3e786c09363560
jiayishen21 <lucasshen21@gmail.com>
2023-08-24 01:08:42 -0400

    bugfixed fetching courses, units, and lessons

--- pages/api/learn/lessons.js
+++ pages/api/learn/lessons.js
@@ -51,7 +51,7 @@ export default async function (req, res) {
       })
       for (let j in user.completedLessons) {
         if (
-          (user.completedLessons[j].lesson._id && user.completedLessons[j]._id.equals(unit.lessons[i]._id)) ||
+          (user.completedLessons[j].lesson._id && user.completedLessons[j].lesson._id.equals(unit.lessons[i]._id)) ||
           user.completedLessons[j].lesson && user.completedLessons[j].lesson.equals(unit.lessons[i]._id)
         ) {
           modifiedLessons[i].stars = user.completedLessons[j].stars
