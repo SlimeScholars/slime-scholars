1ff7538448f596e236ffacf2c15fa998264b0879
jiayishen21 <lucasshen21@gmail.com>
2023-08-24 01:28:40 -0400

    reworked lessons to have quizQuestions instead of quizSections

--- models/lessonModel.js
+++ models/lessonModel.js
@@ -1,5 +1,59 @@
 import { Schema, model, models, mongoose } from 'mongoose'
 
+const sectionSchema = new Schema(
+  {
+    index: {
+      type: Number,
+      required: [true, 'Missing index'],
+    },
+    sectionType: {
+      // 0 for text, 1 for img, 2 for mc, 3 for fill in the blank
+      type: Number,
+      required: [true, 'Missing sectionType'],
+    },
+    sectionNumber: {
+      type: Number,
+      required: [true, 'Missing sectionNumber'],
+    },
+    text: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String, // URL of image
+      required: false,
+    },
+    options: {
+      type: [{
+        type: {
+          option: {
+            type: String,
+            required: [true, 'Missing option'],
+          },
+          correct: {
+            type: Boolean,
+            required: false, // required: true makes it impossible to store false
+          },
+        },
+        required: [true, 'Missing option'],
+        _id: false,
+      }],
+      required: false,
+      default: undefined,
+    },
+    blank: {
+      type: [String],
+      required: false,
+      default: undefined,
+    },
+    afterBlank: {
+      type: String,
+      required: false,
+    },
+    _id: false,
+  },
+)
+
 const lessonSchema = new Schema(
   {
     lessonNumber: {
@@ -17,116 +71,20 @@ const lessonSchema = new Schema(
     },
 
     sections: {
-      type: [{
-        type: {
-          index: {
-            type: Number,
-            required: [true, 'Missing index'],
-          },
-          sectionType: {
-            // 0 for text, 1 for img, 2 for mc, 3 for fill in the blank
-            type: Number,
-            required: [true, 'Missing sectionType'],
-          },
-          sectionNumber: {
-            type: Number,
-            required: [true, 'Missing sectionNumber'],
-          },
-          text: {
-            type: String,
-            required: false,
-          },
-          image: {
-            type: String, // URL of image
-            required: false,
-          },
-          options: {
-            type: [{
-              type: {
-                option: {
-                  type: String,
-                  required: [true, 'Missing option'],
-                },
-                correct: {
-                  type: Boolean,
-                  required: false, // required: true makes it impossible to store false
-                },
-              },
-              required: [true, 'Missing option']
-            }],
-            required: false,
-            default: undefined,
-          },
-          blank: {
-            type: [String],
-            required: false,
-            default: undefined,
-          },
-          afterBlank: {
-            type: String,
-            required: false,
-          },
-        },
-        required: [true, 'Missing section'],
-      }],
+      type: [sectionSchema],
       required: [true, 'Missing sections'],
       default: [],
+      _id: false,
     },
 
-    quizSections: {
+    quizQuestions: {
       type: [{
-        type: {
-          index: {
-            type: Number,
-            required: [true, 'Missing index'],
-          },
-          sectionType: {
-            // 0 for text, 1 for img, 2 for mc, 3 for fill in the blank
-            type: Number,
-            required: [true, 'Missing sectionType'],
-          },
-          sectionNumber: {
-            type: Number,
-            required: [true, 'Missing sectionNumber'],
-          },
-          text: {
-            type: String,
-            required: false,
-          },
-          image: {
-            type: String, // URL of image
-            required: false,
-          },
-          options: {
-            type: [{
-              type: {
-                option: {
-                  type: String,
-                  required: [true, 'Missing option'],
-                },
-                correct: {
-                  type: Boolean,
-                  required: false, // required: true makes it impossible to store false
-                },
-              },
-              required: [true, 'Missing option']
-            }],
-            required: false,
-            default: undefined,
-          },
-          blank: {
-            type: [String],
-            required: false,
-            default: undefined,
-          },
-          afterBlank: {
-            type: String,
-            required: false,
-          },
-        },
-        required: [true, 'Missing section'],
+        type: [sectionSchema],
+        required: [true, 'Missing quizSections'],
+        default: [],
+        _id: false,
       }],
-      required: [true, 'Missing sections'],
+      required: true,
       default: [],
     },
   },
