2fc6fd58ac35c3032262e9c932b22f7602cbe8bb
jiayishen21 <lucasshen21@gmail.com>
2023-08-23 21:15:38 -0400

    made slimes in slime inventory have rounded borders

--- components/play/slimes/SlimeInventory.js
+++ components/play/slimes/SlimeInventory.js
@@ -6,28 +6,33 @@ export default function SlimeInventory({ user, loading, setSlime }) {
       {Array.isArray(user.slimes) &&
         user.slimes.map((slime, index) => {
           return (
-            <div
-              key={index}
-              className={`flex flex-col relative flex-wrap w-full ${
-                gameData.rarityColours[slime.rarity].bg
-              } ${gameData.rarityColours[slime.rarity].bord}`}
-            >
-              <button
-                onClick={() => {
-                  console.log(index);
-                  setSlime(slime);
-                }}
-                className="mb-3"
+            <div className="relative">
+              <div
+                key={index}
+                className="overflow-hidden rounded-lg"
               >
-                <img
-                  src={
-                    "/assets/pfp/slimes/" +
-                    gameData.slimePfps[slime.slimeName].pfp
-                  }
-                  alt="Slime"
-                  className="h-20 w-20 mx-auto"
-                />
-              </button>
+                <div
+                  className={`flex flex-col flex-wrap w-full rounded-lg ${gameData.rarityColours[slime.rarity].bg
+                    } ${gameData.rarityColours[slime.rarity].bord}`}
+                >
+                  <button
+                    onClick={() => {
+                      console.log(index);
+                      setSlime(slime);
+                    }}
+                    className="mb-3"
+                  >
+                    <img
+                      src={
+                        "/assets/pfp/slimes/" +
+                        gameData.slimePfps[slime.slimeName].pfp
+                      }
+                      alt="Slime"
+                      className="h-20 w-20 mx-auto"
+                    />
+                  </button>
+                </div>
+              </div>
               <div className="absolute bg-gray-400 h-5 w-10 -bottom-2.5 inset-x-0 mx-auto rounded-md items-center mt-2">
                 {slime.level === slime.maxLevel ? (
                   <p className="text-center text-xs mt-1">Lvl. MAX </p>
