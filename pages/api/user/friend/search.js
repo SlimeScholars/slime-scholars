be79423cb8c42504ff35f91d394795b834a04660
Alex Xu <alex.xu.205@gmail.com>
2023-08-26 17:17:16 -0400

    friends search changed to frontend render

--- pages/api/user/friend/search.js
+++ pages/api/user/friend/search.js
@@ -1,9 +1,9 @@
-import { mongoose } from 'mongoose'
-import { authenticate } from "../../../../utils/authenticate"
-import { checkUserType } from '../../../../utils/checkUserType'
-import connectDB from '../../../../utils/connectDB'
-import User from '../../../../models/userModel'
-import { getPopulatedPlayer } from '../../../../utils/getPopulatedUser'
+import { mongoose } from "mongoose";
+import { authenticate } from "../../../../utils/authenticate";
+import { checkUserType } from "../../../../utils/checkUserType";
+import connectDB from "../../../../utils/connectDB";
+import User from "../../../../models/userModel";
+import { getPopulatedPlayer } from "../../../../utils/getPopulatedUser";
 
 /**
  * @desc    Look for a friend (based on their username)
@@ -12,40 +12,42 @@ import { getPopulatedPlayer } from '../../../../utils/getPopulatedUser'
  * @param   {string} req.query.username - Words included in the username of the friend
  */
 export default async function (req, res) {
-	try {
-		if (req.method !== 'GET') {
-			throw new Error(`${req.method} is an invalid request method`)
-		}
+  try {
+    if (req.method !== "GET") {
+      throw new Error(`${req.method} is an invalid request method`);
+    }
 
-		// Connect to database
-		await connectDB()
+    // Connect to database
+    await connectDB();
 
-		// Authenticate and get user
-		const user = await authenticate(req.headers.authorization)
+    // Authenticate and get user
+    const user = await authenticate(req.headers.authorization);
 
-		// Make sure user is a student
-		checkUserType(user, 1)
+    // Make sure user is a student
+    checkUserType(user, 1);
 
-		const { username } = req.query
+    const { username } = req.query;
 
-		// Get rid of all characters except spaces, letters, and numbers
-		const cleanedUsername = username.toLowerCase().replace(/[^a-z0-9\s]/g, '')
+    // Get rid of all characters except spaces, letters, and numbers
+    const cleanedUsername = username
+      .toLowerCase()
+      .replace(/[^a-z0-9\s]/g, "")
+      .trim();
 
-		// Make an array of keywords out of all space seperated words
-		const keywords = cleanedUsername.split(' ')
+    // Make an array of keywords out of all space seperated words
+    const keywords = cleanedUsername.split(" ");
 
-		const matchingFriends = []
-		for (let i in user.friends) {
-			for (let keyword of keywords) {
-				if (user.friends[i].username.toLowerCase().includes(keyword)) {
-					matchingFriends.push(user.friends[i])
-				}
-			}
-		}
+    const matchingFriends = [];
+    for (let i in user.friends) {
+      for (let keyword of keywords) {
+        if (user.friends[i].username.toLowerCase().includes(keyword)) {
+          matchingFriends.push(user.friends[i]);
+        }
+      }
+    }
 
-		res.status(200).json({ matchingFriends })
-	} catch (error) {
-		res.status(400).json({ message: error.message })
-	}
+    res.status(200).json({ matchingFriends });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
-
