0014da332728c1232af61ea6740fa7d135cb83eb
jiayishen21 <lucasshen21@gmail.com>
2023-08-29 12:46:30 -0400

    moved sorting features into an exported function in utils

--- utils/getPopulatedUser.js
+++ utils/getPopulatedUser.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel'
 import '../models/slimeModel'
+import { getSortedItems, getSortedSlimes } from './sort'
 
 export const getPopulatedUser = async (userId, config = { lessons: 0, units: 0, courses: 0 }) => {
   const modifiedConfig = {
@@ -64,32 +65,8 @@ export const getPopulatedUser = async (userId, config = { lessons: 0, units: 0, 
     user.sentFriendRequests[i] = populatedRequest
   }
 
-  // Sort items
-  // Priority 1: Non-backgrounds
-  // Priority 2: Alphabetical order
-  user.items.sort((a, b) => {
-    if (a.isBg === b.isBg) {
-      return a.itemName.localeCompare(b.itemName)
-    } else {
-      return a.isBg ? 1 : -1
-    }
-  })
-
-  // Sort the user's slimes
-  user.slimes.sort((a, b) => {
-    // Sort by rarity first
-    const rarityOrder = ['Legendary', 'Epic', 'Rare', 'Uncommon', 'Common']
-    const rarityComparison = rarityOrder.indexOf(a.rarity) - rarityOrder.indexOf(b.rarity)
-
-    if (rarityComparison !== 0) {
-      return rarityComparison
-    } else {
-      // If rarity is the same, sort by baseProduction + bonusProduction
-      const aTotalProduction = a.baseProduction + a.bonusProduction
-      const bTotalProduction = b.baseProduction + b.bonusProduction
-      return bTotalProduction - aTotalProduction // Sort in descending order
-    }
-  })
+  user.items = getSortedItems(user.items)
+  user.slimes = getSortedSlimes(user.slimes)
 
   return user
 }
@@ -106,7 +83,10 @@ export const getPopulatedPlayer = async (userId) => {
       select: '-user -createdAt -updatedAt -__v',
       options: { retainNullValues: true },
     })
+    .lean()
     .exec()
+
+  user.slimes = getSortedSlimes(user.slimes)
 
   return user
 }
