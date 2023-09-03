a3961ed8a74ee4a7a851aa1059d8c9354ae580b3
jiayishen21 <lucasshen21@gmail.com>
2023-09-02 22:43:21 -0400

    do not locally reconfigure eslint error

--- components/play/slimes/RewardsPopUp.jsx
+++ components/play/slimes/RewardsPopUp.jsx
@@ -1,5 +1,3 @@
-/* eslint react/no-unescaped-entities */
-
 import { gameData } from "../../../data/gameData";
 import { useState } from "react";
 export default function RewardsPopUp({ rewardMessages, onClose, rewards }) {
