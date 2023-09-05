58520a2f6089a08e8c0bdd66b6a6df739f0dc4ce
jiayishen21 <lucasshen21@gmail.com>
2023-09-04 23:45:51 -0400

    updated inventory item to use next image

--- components/play/inventory/itemList.jsx
+++ components/play/inventory/itemList.jsx
@@ -1,5 +1,6 @@
 import ItemInventory from "./itemInventory";
 import gameData from "../../../data/gameData";
+import { Fragment } from "react";
 
 export default function ItemList({ items, itemOnClick, setItemOnClick, shopping, user }) {
 
@@ -8,33 +9,35 @@ export default function ItemList({ items, itemOnClick, setItemOnClick, shopping,
     <div className="h-full w-full">
       <div className="bg-white/50 rounded-lg grid xl:grid-cols-5 gap-7 p-7 h-full overflow-y-auto">
         {shopping ? (
-          <div>{Array.isArray(items)&&
-            items.map(item => {
-              // Put an array of owned items
-              if (user.items.includes(item)) {
-                return (
-                  <ItemInventory
-                    key={`item-${index}`}
-                    setItemOnClick={setItemOnClick}
-                    item={item}
-                    itemOnClick={itemOnClick}
-                    owned="true"
-                  />
-                );
-              } else {
-                return (
-                  <ItemInventory
-                    key={`item-${index}`}
-                    setItemOnClick={setItemOnClick}
-                    item={item}
-                    itemOnClick={itemOnClick}
-                  />
-                );
-              }
-            })
-            }</div>
+          <Fragment>
+            {Array.isArray(items) &&
+              items.map(item => {
+                // Put an array of owned items
+                if (user.items.includes(item)) {
+                  return (
+                    <ItemInventory
+                      key={`item-${index}`}
+                      setItemOnClick={setItemOnClick}
+                      item={item}
+                      itemOnClick={itemOnClick}
+                      owned="true"
+                    />
+                  );
+                } else {
+                  return (
+                    <ItemInventory
+                      key={`item-${index}`}
+                      setItemOnClick={setItemOnClick}
+                      item={item}
+                      itemOnClick={itemOnClick}
+                    />
+                  );
+                }
+              })
+            }
+          </Fragment>
         ) : (
-          <div>{
+          <Fragment>{
             Array.isArray(items) ? (
               items.map((item, index) => {
                 return (
@@ -49,9 +52,8 @@ export default function ItemList({ items, itemOnClick, setItemOnClick, shopping,
             ) : (
               <p>No items in inventory.</p>
             )}
-          </div>
-        )
-        }
+          </Fragment>
+        )}
       </div>
     </div>
   );
