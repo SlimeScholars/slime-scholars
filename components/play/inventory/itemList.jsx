83de4ae8e9992028c6bfcaa4bdd5b4e12d06c4a3
huang9185 <helyn20012@outlook.com>
2023-09-04 23:01:29 -0400

    Recreating shopping page

--- components/play/inventory/itemList.jsx
+++ components/play/inventory/itemList.jsx
@@ -1,23 +1,57 @@
 import ItemInventory from "./itemInventory";
+import gameData from "../../../data/gameData";
 
-export default function ItemList({ items, itemOnClick, setItemOnClick }) {
+export default function ItemList({ items, itemOnClick, setItemOnClick, shopping, user }) {
+
+  console.log(items);
   return (
     <div className="h-full w-full">
       <div className="bg-white/50 rounded-lg grid xl:grid-cols-5 gap-7 p-7 h-full overflow-y-auto">
-        {Array.isArray(items) ? (
-          items.map((item, index) => {
-            return (
-              <ItemInventory
-                key={`item-${index}`}
-                setItemOnClick={setItemOnClick}
-                item={item}
-                itemOnClick={itemOnClick}
-              />
-            );
-          })
+        {shopping ? (
+          <div>{Array.isArray(items)&&
+            items.map(item => {
+              // Put an array of owned items
+              if (user.items.includes(item)) {
+                return (
+                  <ItemInventory
+                    key={`item-${index}`}
+                    setItemOnClick={setItemOnClick}
+                    item={item}
+                    itemOnClick={itemOnClick}
+                    owned="true"
+                  />
+                );
+              } else {
+                return (
+                  <ItemInventory
+                    key={`item-${index}`}
+                    setItemOnClick={setItemOnClick}
+                    item={item}
+                    itemOnClick={itemOnClick}
+                  />
+                );
+              }
+            })
+            }</div>
         ) : (
-          <p>No items in inventory.</p>
-        )}
+          <div>{
+            Array.isArray(items) ? (
+              items.map((item, index) => {
+                return (
+                  <ItemInventory
+                    key={`item-${index}`}
+                    setItemOnClick={setItemOnClick}
+                    item={item}
+                    itemOnClick={itemOnClick}
+                  />
+                );
+              })
+            ) : (
+              <p>No items in inventory.</p>
+            )}
+          </div>
+        )
+        }
       </div>
     </div>
   );
