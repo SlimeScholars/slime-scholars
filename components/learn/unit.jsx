694d316876b2db190d9f1d309bac721b4591860c
jiayishen21 <lucasshen21@gmail.com>
2023-09-05 16:16:58 -0400

    updated spacing between lessons units and courses on lesson selection

--- components/learn/unit.jsx
+++ components/learn/unit.jsx
@@ -13,7 +13,7 @@ export default function Unit({ courseId, unitId, unitName, unitBadge, tier }) {
 
 	return (
 		<button
-			className={`w-[80%] h-[7rem] text-3xl flex justify-start items-center ${tierClass[tier]}`}
+			className={`w-[80%] h-[7rem] text-3xl flex justify-start items-center ${tierClass[tier]} mb-7`}
 			onClick={() => {
 				router.push(`/courses/${courseId}/units/${unitId}/lessons`)
 			}}
