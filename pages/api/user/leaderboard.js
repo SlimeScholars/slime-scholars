c03fc14441403ba4af2170f7183c7c2f00a177ff
Alex Xu <alex.xu.205@gmail.com>
2023-09-10 15:28:30 -0400

    show user in leaderboard even if not in top 20

--- pages/api/user/leaderboard.js
+++ pages/api/user/leaderboard.js
@@ -1,44 +1,64 @@
-import User from "../../../models/userModel"
-import { authenticate } from "../../../utils/authenticate"
-import { checkUserType } from "../../../utils/checkUserType"
-import connectDB from '../../../utils/connectDB'
-import { getPopulatedPlayer } from "../../../utils/getPopulatedUser"
+import User from "../../../models/userModel";
+import { authenticate } from "../../../utils/authenticate";
+import { checkUserType } from "../../../utils/checkUserType";
+import connectDB from "../../../utils/connectDB";
+import { getPopulatedPlayer } from "../../../utils/getPopulatedUser";
 
 /**
  * @desc    Get user data of the person who is signed in
  * @route   GET /api/user/leaderboard
- * @access  Private - Any logged in student
+ * @access  Private - Any logged-in student
  */
 export default async function (req, res) {
-	try {
-		if (req.method !== 'GET') {
-			throw new Error(`${req.method} is an invalid request method`)
-		}
+  try {
+    if (req.method !== "GET") {
+      throw new Error(`${req.method} is an invalid request method`);
+    }
 
-		// Connect to database
-		await connectDB()
+    // Connect to the database
+    await connectDB();
 
-		// Authenticate and get user
-		const user = await authenticate(req.headers.authorization)
+    // Authenticate and get user
+    const user = await authenticate(req.headers.authorization);
 
-		// Make sure user is a student
-		checkUserType(user, 1)
+    // Make sure user is a student
+    checkUserType(user, 1);
 
-		const leaderboard = await User.find({ userType: 1 })
-			.sort({ exp: -1 }) // Sort in descending order by exp
-			// TODO: Make sure 20 is the number to show
-			.limit(20) // How many users to find
-			.select('_id')
+    // Find the top 10 users with the highest experience, sorted in descending order
+    const leaderboard = await User.find({ userType: 1 })
+      .sort({ exp: -1 })
+      .select("_id");
 
-		const populatedLeaderboard = []
+    // Find the user's rank in the sorted leaderboard
+    const userRank = leaderboard.findIndex(
+      (leader) => leader._id.toString() === user._id.toString()
+    );
+    const actualUserRank = userRank + 1;
 
-		for (let i in leaderboard) {
-			const populatedPlayer = await getPopulatedPlayer(leaderboard[i]._id)
-			populatedLeaderboard.push(populatedPlayer)
-		}
+    const sortedLeaderboard = await User.find({ userType: 1 })
+      .sort({ exp: -1 })
+      .limit(10)
+      .select("_id");
+    const populatedLeaderboard = [];
 
-		res.status(200).json({ leaderboard: populatedLeaderboard })
-	} catch (error) {
-		res.status(400).json({ message: error.message })
-	}
+    // Loop through the leaderboard and populate each player
+    for (let i = 0; i < sortedLeaderboard.length; i++) {
+      const populatedPlayer = await getPopulatedPlayer(
+        sortedLeaderboard[i]._id
+      );
+      populatedLeaderboard.push(populatedPlayer);
+    }
+
+    // If the user is not in the top 10, populate the user's data and add it to the leaderboard
+    if (actualUserRank >= 10) {
+      const populatedPlayer = await getPopulatedPlayer(user._id);
+      populatedLeaderboard.push(populatedPlayer);
+    }
+
+    res
+      .status(200)
+      .json({ leaderboard: populatedLeaderboard, userRank: actualUserRank });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
