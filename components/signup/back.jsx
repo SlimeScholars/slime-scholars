d511f91c6e57bb8b345754a31d0e1c91d71ffd2f
Anthony Qiu <aq0335@gmail.com>
2023-09-16 18:14:42 -0400

    changed styling

--- components/signup/back.jsx
+++ components/signup/back.jsx
@@ -5,6 +5,8 @@ import { HiHome } from "react-icons/hi";
 export default function Back({to}) {
   return (
     <nav className="w-screen h-32 flex justify-start items-center fixed top-0">
+      {/*
+      FIX ME
       <a
         href={to}
         className="h-full flex justify-start items-center px-10 stretch"
@@ -15,6 +17,7 @@ export default function Back({to}) {
         </p>
         <HiHome className="text-3xl text-bg-light/90" />
       </a>
+    */}
     </nav>
   );
 }
