33932ceaa345e30ad2c219a8475f45db06473713
jiayishen21 <lucasshen21@gmail.com>
2023-09-17 13:05:31 -0400

    enabled back to home on settings

--- components/signup/back.jsx
+++ components/signup/back.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import { FaChevronCircleLeft } from "react-icons/fa";
 import { HiHome } from "react-icons/hi";
 
-export default function Back({to}) {
+export default function Back({ to }) {
   return (
     <nav className="w-screen h-32 flex justify-start items-center fixed top-0">
-      {/*
-      FIX ME
       <a
         href={to}
         className="h-full flex justify-start items-center px-10 stretch"
@@ -17,7 +15,7 @@ export default function Back({to}) {
         </p>
         <HiHome className="text-3xl text-bg-light/90" />
       </a>
-    */}
+
     </nav>
   );
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -52,7 +52,7 @@ export default function Login({ loading, user, setUser }) {
 
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center bg-[url('/assets/backgrounds/bg-galaxy.png')] bg-no-repeat bg-cover">
-      <Back to={"/"} />
+      {/* FIXME <Back to={"/"} /> */}
       <div className="w-1/3 bg-gradient-to-br from-blue-400/70 to-purple-900/70 opacity-90 rounded-2xl p-3">
         <form
           className="w-full h-full bg-indigo-950/80 rounded-lg px-14 py-10 flex flex-col items-center justify-between overflow-hidden"
