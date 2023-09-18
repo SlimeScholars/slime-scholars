93d8f76696b12fc22e8386cddaffc1a41f760548
John Liu <johnliu@v1041-wn-rt-b-173-254.campus-dynamic.uwaterloo.ca>
2023-09-18 15:53:16 -0400

    navbar

--- components/play/Navbar.jsx
+++ components/play/Navbar.jsx
@@ -55,217 +55,201 @@ export function Navbar({
   }, [user]);
 
   return (
-    <div
-      className="grid items-center justify-between z-20 w-full relative"
-      style={{
-        gridTemplateColumns: onHome ? "12rem 1fr" : "9rem 12rem 1fr",
-      }}
-    >
-      {/* home button */}
-      {!onHome && (
+    <div className="relative flex flex-col gap-4 z-20 items-end">
+      <div
+        className="grid items-center justify-between w-full"
+        style={{
+          gridTemplateColumns: onHome ? "12rem 1fr" : "9rem 12rem 1fr",
+        }}
+      >
+        {/* home button */}
+        {!onHome && (
+          <button
+            className="rounded hover:opacity-80 font-galindo mr-6 h-[4rem] transition-opacity duration-150"
+            style={{
+              backgroundColor:
+                !colorPalette ? "" : colorPalette.primary1,
+              color: !colorPalette ? "" : colorPalette.text1,
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/play");
+            }}
+          >
+            <FaChevronLeft className="inline text-lg" />
+            <HiHome className="inline text-3xl ml-1 -mt-0.5 mr-3" />
+          </button>
+        )}
+        {/* earn flowers button */}
         <button
-          className="rounded hover:opacity-80 font-galindo mr-6 h-[4rem] transition-opacity duration-150"
+          className="rounded-md brightness-[1.05] hover:brightness-[1] font-galindo text-lg h-[4rem] transition-brightness duration-150"
           style={{
             backgroundColor:
               !colorPalette ? "" : colorPalette.primary1,
             color: !colorPalette ? "" : colorPalette.text1,
+            boxShadow: !colorPalette ? "" : `0 0 2px ${colorPalette.primary2}`
           }}
           onClick={(e) => {
             e.preventDefault();
-            router.push("/play");
+            router.push("/play/courses");
           }}
         >
-          <FaChevronLeft className="inline text-lg" />
-          <HiHome className="inline text-3xl ml-1 -mt-0.5 mr-3" />
+          Earn Flowers
         </button>
-      )}
-      {/* earn flowers button */}
-      <button
-        className="rounded-md brightness-[1.05] hover:brightness-[1] font-galindo text-lg h-[4rem] transition-brightness duration-150"
-        style={{
-          backgroundColor:
-            !colorPalette ? "" : colorPalette.primary1,
-          color: !colorPalette ? "" : colorPalette.text1,
-          boxShadow: !colorPalette ? "" : `0 0 2px ${colorPalette.primary2}`
-        }}
-        onClick={(e) => {
-          e.preventDefault();
-          router.push("/play/courses");
-        }}
-      >
-        Earn Flowers
-      </button>
-      <div className="flex flex-row items-center space-x-2 justify-end font-galindo 2xl:text-lg text-md">
-        <div className="flex flex-col items-end mr-5">
-          {/* slime gel */}
-          <div
-            className="flex rounded-full py-1 px-6 w-fit"
-            style={{
-              backgroundColor: `${colorPalette ? colorPalette.black + "55" : "#475569"
-                }`,
-              color: `${colorPalette ? colorPalette.text1 : "#ffffff"}`,
-              boxShadow: `${colorPalette ? "0px 0px 20px " + colorPalette.white + "0F" : ""
-                }`,
-            }}
-          >
-            {user && (
-              <div className="flex flex-row items-center">
-                <Image
-                  src="/assets/icons/slime-gel.png"
-                  alt="slime gel"
-                  height={0}
-                  width={0}
-                  sizes="100vw"
-                  className="2xl:h-[1.7rem] 2xl:w-[1.7rem] h-[1.4rem] w-[1.4rem] 2xl:ml-1 mr-2 -mt-0.5"
-                />
-                <p className="">{user.slimeGel}</p>
-              </div>
-            )}
-          </div>
-
-          {/* flowers */}
-          <div
-            className="flex rounded-full py-1 px-6 w-fit mt-1.5"
-            style={{
-              backgroundColor: `${colorPalette ? colorPalette.black + "55" : "#475569"
-                }`,
-              color: `${colorPalette ? colorPalette.text1 : "#ffffff"}`,
-              boxShadow: `${colorPalette ? "0px 0px 20px " + colorPalette.white + "0F" : ""
-                }`,
-            }}
-          >
-            {user && (
-              <div className="flex flex-row items-center">
-                <Image
-                  src="/assets/icons/flower.png"
-                  alt="flowers"
-                  height={0}
-                  width={0}
-                  sizes="100vw"
-                  className="2xl:h-[1.7rem] 2xl:w-[1.7rem] h-[1.4rem] w-[1.4rem] 2xl:ml-1 mr-2 -mt-0.5"
-                />
-                <p className="">{flowers === null ? user.flowers : flowers}</p>
-              </div>
-            )}
-          </div>
-
-          {/*
-          FIXME
-          Slime Egg
-          {
-            (user && current_id === 5) && (
-              <div className="flex bg-white/50 opacity-60 rounded-md mt-1 p-1">
-                {user && (
-                  <div className="flex flex-row items-center">
-                    <img
-                      src="/assets/icons/slime-egg.png"
-                      alt="Icon"
-                      className="h-4 w-4 ml-1 mr-2"
-                    />
-                    <p className="text-black text-sm">{numEggs}</p>
-                  </div>
-                )}
-              </div>
-            )
-          }
-        */}
-        </div>
-
-        {/* buttons and icons */}
-        {types.map((type) => {
-          const imgLink = "/assets/icons/" + type.src + ".png";
-          const isActive = type.id === current_id;
-
-          return (
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                router.push("/play/" + type.title.toLowerCase());
+        <div className="flex flex-row items-center space-x-2 justify-end font-galindo 2xl:text-lg text-md">
+          <div className="flex flex-col items-end mr-5">
+            {/* slime gel */}
+            <div
+              className="flex rounded-full py-1 px-6 w-fit"
+              style={{
+                backgroundColor: `${colorPalette ? colorPalette.black + "55" : "#475569"
+                  }`,
+                color: `${colorPalette ? colorPalette.text1 : "#ffffff"}`,
+                boxShadow: `${colorPalette ? "0px 0px 20px " + colorPalette.white + "0F" : ""
+                  }`,
               }}
-              style={
-                isActive
-                  ? {
-                    backgroundColor: `${colorPalette ? colorPalette.primary1 : "#ffffff"
-                      }`,
-                    border: `${type.id === 6 && colorPalette !== undefined
-                      ? `5px solid ${colorPalette.primary1}`
-                      : ""
-                      }`,
-                    padding: type.id === 6 ? "0.6rem" : undefined, // Add padding only when type.id is 6
-                  }
-                  : {
-                    backgroundColor: `${colorPalette ? colorPalette.white : "#ffff"
-                      }88`,
-                    border: `${type.id === 6 && colorPalette !== undefined
-                      ? `5px solid ${colorPalette.primary1}`
-                      : ""
-                      }`,
-                    padding: type.id === 6 ? "0.6rem" : undefined, // Add padding only when type.id is 6
-                  }
-              }
-              className={`hover:opacity-60 rounded-full p-3 overflow-hidden relative box-border ${type.id === 6 ? "2xl:w-[5.6rem] 2xl:h-[5.6rem]" : "" // Apply larger size for 2xl screen and type.id 6
-                }`}
-              key={type.id}
-              data-tooltip-id="my-tooltip"
-              data-tooltip-content={type.title}
             >
-              {type.id === 6 ? (
-                user && user.pfpSlime ? (
-                  <div className="relative flex items-center justify-center">
-                    <div className="absolute h-32 w-32 overflow-hidden"
-                    >
+              {user && (
+                <div className="flex flex-row items-center">
+                  <Image
+                    src="/assets/icons/slime-gel.png"
+                    alt="slime gel"
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="2xl:h-[1.7rem] 2xl:w-[1.7rem] h-[1.4rem] w-[1.4rem] 2xl:ml-1 mr-2 -mt-0.5"
+                  />
+                  <p className="">{user.slimeGel}</p>
+                </div>
+              )}
+            </div>
+
+            {/* flowers */}
+            <div
+              className="flex rounded-full py-1 px-6 w-fit mt-1.5"
+              style={{
+                backgroundColor: `${colorPalette ? colorPalette.black + "55" : "#475569"
+                  }`,
+                color: `${colorPalette ? colorPalette.text1 : "#ffffff"}`,
+                boxShadow: `${colorPalette ? "0px 0px 20px " + colorPalette.white + "0F" : ""
+                  }`,
+              }}
+            >
+              {user && (
+                <div className="flex flex-row items-center">
+                  <Image
+                    src="/assets/icons/flower.png"
+                    alt="flowers"
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="2xl:h-[1.7rem] 2xl:w-[1.7rem] h-[1.4rem] w-[1.4rem] 2xl:ml-1 mr-2 -mt-0.5"
+                  />
+                  <p className="">{flowers === null ? user.flowers : flowers}</p>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* buttons and icons */}
+          {types.map((type) => {
+            const imgLink = "/assets/icons/" + type.src + ".png";
+            const isActive = type.id === current_id;
+
+            return (
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  router.push("/play/" + type.title.toLowerCase());
+                }}
+                style={
+                  isActive
+                    ? {
+                      backgroundColor: `${colorPalette ? colorPalette.primary1 : "#ffffff"
+                        }`,
+                      border: `${type.id === 6 && colorPalette !== undefined
+                        ? `5px solid ${colorPalette.primary1}`
+                        : ""
+                        }`,
+                      padding: type.id === 6 ? "0.6rem" : undefined, // Add padding only when type.id is 6
+                    }
+                    : {
+                      backgroundColor: `${colorPalette ? colorPalette.white : "#ffff"
+                        }88`,
+                      border: `${type.id === 6 && colorPalette !== undefined
+                        ? `5px solid ${colorPalette.primary1}`
+                        : ""
+                        }`,
+                      padding: type.id === 6 ? "0.6rem" : undefined, // Add padding only when type.id is 6
+                    }
+                }
+                className={`hover:opacity-60 rounded-full p-3 overflow-hidden relative box-border ${type.id === 6 ? "2xl:w-[5.6rem] 2xl:h-[5.6rem]" : "" // Apply larger size for 2xl screen and type.id 6
+                  }`}
+                key={type.id}
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={type.title}
+              >
+                {type.id === 6 ? (
+                  user && user.pfpSlime ? (
+                    <div className="relative flex items-center justify-center">
+                      <div className="absolute h-32 w-32 overflow-hidden"
+                      >
+                        <Image
+                          src={
+                            "/assets/pfp/backgrounds/" + gameData.items[user.pfpBg].pfp
+                          }
+                          alt={user.pfpBg}
+                          height={0}
+                          width={0}
+                          sizes="100vw"
+                          className="absolute h-32 w-32"
+                        />
+                      </div>
                       <Image
                         src={
-                          "/assets/pfp/backgrounds/" + gameData.items[user.pfpBg].pfp
+                          "/assets/pfp/slimes/" +
+                          gameData.slimes[user.pfpSlime].pfp
                         }
-                        alt={user.pfpBg}
+                        alt={user.pfpSlime}
                         height={0}
                         width={0}
                         sizes="100vw"
-                        className="absolute h-32 w-32"
+                        className="relative z-10 translate-y-1/3 scale-150 w-[3.5rem] h-[3.5rem]"
                       />
                     </div>
-                    <Image
-                      src={
-                        "/assets/pfp/slimes/" +
-                        gameData.slimes[user.pfpSlime].pfp
-                      }
-                      alt={user.pfpSlime}
-                      height={0}
-                      width={0}
-                      sizes="100vw"
-                      className="relative z-10 translate-y-1/3 scale-150 w-[3.5rem] h-[3.5rem]"
-                    />
-                  </div>
+                  ) : (
+                    // Handle the case when user or user.pfpSlime is null or undefined
+                    <div className="default-image">
+                      {"/assets/pfp/slimes/blue-slime.png"}
+                    </div>
+                  )
                 ) : (
-                  // Handle the case when user or user.pfpSlime is null or undefined
-                  <div className="default-image">
-                    {"/assets/pfp/slimes/blue-slime.png"}
-                  </div>
-                )
-              ) : (
-                <Image
-                  src={imgLink}
-                  alt={type.src}
-                  height={0}
-                  width={0}
-                  sizes="100vw"
-                  className="2xl:h-[4rem] 2xl:w-[4rem] h-[3.5rem] w-[3.5rem]"
+                  <Image
+                    src={imgLink}
+                    alt={type.src}
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="2xl:h-[4rem] 2xl:w-[4rem] h-[3.5rem] w-[3.5rem]"
+                  />
+                )}
+                <Tooltip
+                  id="my-tooltip"
+                  delayShow={200}
+                  place="bottom"
+                  pffset={20}
+                  style={{
+                    backgroundColor: "#2c374240",
+                    fontSize: "14px",
+                  }}
                 />
-              )}
-              <Tooltip
-                id="my-tooltip"
-                delayShow={200}
-                place="bottom"
-                pffset={20}
-                style={{
-                  backgroundColor: "#2c374240",
-                  fontSize: "14px",
-                }}
-              />
-            </button>
-          );
-        })}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="">
+          
       </div>
     </div>
   );
