2a32b022d673cc9e7d441cb8155f3f1073a1277d
Anthony Qiu <aq0335@gmail.com>
2023-09-21 15:07:07 -0400

    created page for mobile users and fixed nav bar due to merge issues

--- pages/mobile.jsx
+++ pages/mobile.jsx
@@ -1,7 +1,10 @@
 import { useRouter } from "next/router"
 import { useEffect } from "react"
+import Head from 'next/head'
+import Nav from '../components/homepage/nav'
+import Button from '../components/homepage/button'
 
-export default function Mobile({ isMobile, user }) {
+export default function Mobile({ isMobile, user, setUser}) {
   const router = useRouter()
 
   useEffect(() => {
@@ -19,7 +22,31 @@ export default function Mobile({ isMobile, user }) {
   }, [isMobile])
   return (
     <>
-      LOL
+      <div className="w-screen flex flex-col mt-5">
+      <Head>
+        <title>Slime Scholars</title>
+        <meta name="description" content="Make Learning Exiting with Slime Scholars" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="w-screen flex flex-col items-center justify-center flex-1">
+        <Nav
+          user={user}
+          setUser={setUser}
+        />
+        <header className="w-screen h-screen flex flex-col items-center justify-center">
+          <h1 className="font-black text-6xl max-w-4xl text-center mt-2">
+            Make Learning Exciting with Slime Scholars
+          </h1>
+          <h2 className="font-light text-3xl my-6 text-center max-w-4xl leading-relaxed text-gray-600">
+            Slime Scholars is a project that motivates students to learn by making the process gratifying.
+          </h2>
+          <h2 className="font-galindo text-4xl">
+            Coming to <span className="text-pink-500">appstore</span> soon!
+          </h2>
+        </header>
+      </main>
+    </div>
     </>
   )
 }
