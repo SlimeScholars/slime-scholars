a3c08473cb2c133d1b2c5a222bb0ec28109e9c3e
John Liu <johnliu@v1040-wn-rt-a-51-117.campus-dynamic.uwaterloo.ca>
2023-09-22 15:11:17 -0400

    non-quiz db commit

---
+++ models/activityModel.js
@@ -0,0 +1,37 @@
+import { Schema, model, models, } from 'mongoose'
+
+const activitySchema = new Schema(
+  {
+    activityNumber: {
+      type: Number,
+      required: [true, 'Missing unitNumber'],
+    },
+    activityName: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    latestAuthor: {
+      type: String,
+      required: [true, 'Missing latestAuthor'],
+    },
+
+    pages: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Page',
+            required: [true, 'Missing pageId'],
+          }],
+      required: [true, 'Missing pages'],
+      default: [],
+      _id: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+const Activity = models.Activity || model('Activity', activitySchema)
+
+export default Activity
