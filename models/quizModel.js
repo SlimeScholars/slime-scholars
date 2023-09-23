cf99f43a092269bbae4b88f0994ebd0b35c61df5
Alex Xu <alex.xu.205@gmail.com>
2023-09-23 16:14:43 -0400

    quiz model: quizNum, quizName, pages (sections)

--- models/quizModel.js
+++ models/quizModel.js
@@ -1,3 +1,43 @@
 //TODO: POP QUIZ / UNIT TEST
 
 //unit -> this -> quiz, test
+
+import { Schema, model, models, mongoose } from "mongoose";
+
+const quizSchema = new Schema(
+  {
+    quizNumber: {
+      type: Number,
+      required: [true, "Missing unitNumber"],
+    },
+    quizName: {
+      type: String,
+      default: "",
+      required: false,
+    },
+    latestAuthor: {
+      type: String,
+      required: [true, "Missing latestAuthor"],
+    },
+
+    pages: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Page",
+          required: [true, "Missing pageId"],
+        },
+      ],
+      required: [true, "Missing pages"],
+      default: [],
+      _id: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Quiz = models.Quiz || model("Quiz", quizSchema);
+
+export default Quiz;
