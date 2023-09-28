d11fea148e47cfd51e699877ed7e4caf561641f2
Anthony Qiu <aq0335@gmail.com>
2023-09-27 20:27:23 -0400

    Fixed mobile screen

--- pages/play/settings.jsx
+++ pages/play/settings.jsx
@@ -124,7 +124,7 @@ export default function Settings({ loading, user, setUser }) {
   }
 
   return (
-    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-[url('/assets/backgrounds/bg-galaxy.png')]">
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-no-repeat bg-[url('/assets/backgrounds/bg-galaxy.png')]">
       <Back to={"/play"} />
       <div className="w-1/2 bg-gradient-to-br from-blue-400/70 to-purple-900/70 opacity-90 rounded-2xl p-3">
         <form className="relative w-full h-full bg-indigo-950/80 rounded-lg px-14 py-10 flex flex-col items-center justify-between overflow-hidden">
