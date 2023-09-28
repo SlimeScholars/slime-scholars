0625c45aa9e688b34b461bcf9ccf14ec7fdacc4d
Anthony Qiu <aq0335@gmail.com>
2023-09-27 20:36:17 -0400

    fixed marign for mobile

--- components/homepage/nav.jsx
+++ components/homepage/nav.jsx
@@ -38,7 +38,7 @@ export default function Nav({ user, setUser }) {
   }, [user]);
 
   return (
-    <nav className="w-screen static top-0 h-18 bg-transparent flex flex-row justify-between items-center px-4 lg:px-10 -mb-[4rem]">
+    <nav className="w-screen static top-0 h-18 bg-transparent flex flex-row justify-between items-center px-4 lg:px-10 -mb-[2rem]">
       <a className="flex justify-center items-center h-auto" href="/">
         <Image
           src="/assets/icons/logo.png"
