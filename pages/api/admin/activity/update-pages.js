fcb1c7205434b6c20abb2e0a32fa5574b01f5e18
Alex Xu <alex.xu.205@gmail.com>
2023-09-30 21:50:01 -0400

    update pages

--- pages/api/admin/activity/update-pages.js
+++ pages/api/admin/activity/update-pages.js
@@ -1,11 +1,14 @@
-import { authenticate } from "../../../../utils/authenticate"
-import { checkUserType } from '../../../../utils/checkUserType'
-import connectDB from '../../../../utils/connectDB'
-import Activity from "../../../../models/activityModel"
-import { processMarkdown, verifyNesting } from "../../../../utils/processMarkdown"
+import { authenticate } from "../../../../utils/authenticate";
+import { checkUserType } from "../../../../utils/checkUserType";
+import connectDB from "../../../../utils/connectDB";
+import Activity from "../../../../models/activityModel";
+import {
+  processMarkdown,
+  verifyNesting,
+} from "../../../../utils/processMarkdown";
 // TODO: investigate the ts error
-import formidable from 'formidable-serverless';
-import { v2 as cloudinary } from 'cloudinary';
+import formidable from "formidable-serverless";
+import { v2 as cloudinary } from "cloudinary";
 
 export const config = {
   api: {
@@ -18,26 +21,26 @@ export const config = {
  * @route   PUT /api/admin/activity/update-sections
  * @access  Private - Admin
  * @param   {string} req.body.activityId - Id of activity you want to update
- * @param   {string} req.body.sections - Sections you want to update activity to 
+ * @param   {string} req.body.sections - Sections you want to update activity to
  * @param   {string} req.body.imageLength - Number of images being uploaded
  */
 export default async function (req, res) {
   try {
-    if (req.method !== 'PUT') {
-      throw new Error(`${req.method} is an invalid request method`)
+    if (req.method !== "PUT") {
+      throw new Error(`${req.method} is an invalid request method`);
     }
 
     // Connect to database
-    await connectDB()
+    await connectDB();
 
     // Authenticate and get user
-    const user = await authenticate(req.headers.authorization)
+    const user = await authenticate(req.headers.authorization);
 
     // Make sure user is a teacher
-    checkUserType(user, 4)
+    checkUserType(user, 4);
 
     const form = new formidable.IncomingForm();
-    form.keepExtensions = true
+    form.keepExtensions = true;
     const data = await new Promise((resolve, reject) => {
       form.parse(req, (err, fields, files) => {
         if (err) {
@@ -48,162 +51,130 @@ export default async function (req, res) {
       });
     });
 
-    const { activityId, pages, pageIndex, imageLength } = JSON.parse(data.fields.data)
+    const imageFiles = [];
+    for (let i = 0; i < imageLength; i++) {
+      if (data.files && data.files[`image${i} `]) {
+        imageFiles.push(data.files[`image${i} `]);
+      }
+    }
+
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
+    });
+
+    const uploadedImages = [];
+
+    for (const image of imageFiles) {
+      // Upload the file to Cloudinary
+      await cloudinary.uploader.upload(image.path, (error, result) => {
+        if (error) {
+          throw new Error(`Error uploading file: ${error} `);
+        } else {
+          uploadedImages.push(result.secure_url);
+        }
+      });
+    }
+
+    const { activityId, pages, pageIndex, imageLength } = JSON.parse(
+      data.fields.data
+    );
+
+    if (!activityId) {
+      throw new Error("Please send a activityId");
+    }
+
+    const activityExists = Activity.findById(activityId);
+
+    if (!activityExists) {
+      throw new Error("Could not find the activity to update");
+    }
 
     if (!pages) {
-      throw new Error('Please send sections')
+      throw new Error("Please send sections");
     }
     if (imageLength === undefined) {
-      throw new Error('Please send imageLength')
+      throw new Error("Please send imageLength");
     }
 
     for (let i in pages) {
       if (i + 1 !== pages[i].pageNumber) {
-        throw new Error('Page numbers must be sequential')
+        throw new Error("Page numbers must be sequential");
       }
     }
 
     if (!pages[pageIndex]?.sections) {
-      throw new Error('Please send sections')
+      throw new Error("Please send sections");
     }
-    const sections = pages[pageIndex].sections
+
+    const sections = pages[pageIndex].sections;
+    const processedPages = [];
 
     for (let i in sections) {
       if (i !== sections[i].sectionIndex) {
-        throw new Error('Section numbers must be sequential')
+        throw new Error("Section numbers must be sequential");
       }
 
-      const section = { ...sections[i] }
+      const section = { ...sections[i] };
 
-      const processedElements = []
+      const processedElements = [];
       for (let j in section.elements) {
-        const element = { ...section.elements[j] }
-        if (!Number.isInteger(element.elementType) || element.sectionNumber < 0 || element.elementType > 3) {
-          throw new Error('Element type is invalid')
+        const element = { ...section.elements[j] };
+        if (
+          !Number.isInteger(element.elementType) ||
+          element.sectionNumber < 0 ||
+          element.elementType > 3
+        ) {
+          throw new Error("Element type is invalid");
         }
 
         const processedElement = {
           index: element.index,
           elementType: element.elementType,
-        }
+        };
 
         // text
         if (element.elementType === 0) {
-          const processedText = processMarkdown(element.text)
+          const processedText = processMarkdown(element.text);
           if (!verifyNesting(processedText)) {
-            throw new Error('Must close styles in correct order. eg. *b* bold *b* is correct, *b* bold *i* *b* *i* is not.')
+            throw new Error(
+              "Must close styles in correct order. eg. *b* bold *b* is correct, *b* bold *i* *b* *i* is not."
+            );
           }
-          processedElement.text = processedText
+          processedElement.text = processedText;
         }
+        //img
+        else if (element.elementType === 1) {
+          processedElement.image = element.image;
+        }
+        //multiple choice
+        else if (element.elementType === 2) {
+          processedElement.options = element.options;
+          processedElement.explanation = element.explanation;
+        }
+        //fill in the blank
+        else if (element.elementType === 3) {
+          processedElement.text = element.text;
+          processedElement.afterBlank = element.afterBlank;
+          const rawBlank = element.blank.split(",");
+          processedElement.blank = rawBlank.map((str) => str.trim());
+          processedElement.explanation = element.explanation;
+        }
+        processedElements.push(processedElement);
       }
-
-      // Make sure there is only one question max section number
-      // const sectionsOverlap = {}
-      // for (let i in sections) {
-      //   if (sections.sectionType === 2 || sections.sectionType === 3) {
-      //     if (sectionsOverlap[sections[i].sectionNumber]) {
-      //       throw new Error(`There can only one question max per section number.On activity section number ${sections[i].sectionNumber} there is an overlap.`)
-      //     }
-      //     sectionsOverlap[sections[i].sectionNumber] = true
-      //   }
-      // }
-
-      // const imageFiles = []
-      // for (let i = 0; i < imageLength; i++) {
-      //   if (data.files && data.files[`image${i} `]) {
-      //     imageFiles.push(data.files[`image${i} `])
-      //   }
-      // }
-
-      // cloudinary.config({
-      //   cloud_name: process.env.CLOUD_NAME,
-      //   api_key: process.env.API_KEY,
-      //   api_secret: process.env.API_SECRET,
-      // })
-
-      // const uploadedImages = []
-
-      // for (const image of imageFiles) {
-      //   // Upload the file to Cloudinary
-      //   await cloudinary.uploader.upload(image.path, (error, result) => {
-      //     if (error) {
-      //       throw new Error(`Error uploading file: ${error} `);
-      //     } else {
-      //       uploadedImages.push(result.secure_url)
-      //     }
-      //   });
-      // }
-
-      // for (let i in sections) {
-      //   if (sections[i].sectionType === 1 &&
-      //     typeof sections[i].image !== 'string') {
-      //     sections[i].image = uploadedImages[sections[i].image]
-      //   }
-      // }
-
-      // if (!activityId) {
-      //   throw new Error('Please send a activityId')
-      // }
-
-      // const activityExists = Activity.findById(activityId)
-
-      // if (!activityExists) {
-      //   throw new Error('Could not find the activity to update')
-      // }
-
-      // const processedSections = []
-      // for (let section of sections) {
-      //   if (!Number.isInteger(section.sectionNumber) || section.sectionNumber < 0) {
-      //     throw new Error('Section numbers must all be positive integers (you may have left a section number blank).')
-      //   }
-
-      //   const processedSection = {
-      //     index: section.index,
-      //     sectionType: section.sectionType,
-      //     sectionNumber: section.sectionNumber,
-      //   }
-
-      //   //text
-      //   if (section.sectionType === 0) {
-      //     const processedText = processMarkdown(section.text)
-      //     if (!verifyNesting(processedText)) {
-      //       throw new Error('Must close styles in correct order. eg. *b* bold *b* is correct, *b* bold *i* *b* *i* is not.')
-      //     }
-      //     processedSection.text = processedText
-      //   }
-      //   //img
-      //   else if (section.sectionType === 1) {
-      //     processedSection.image = section.image
-      //   }
-      //   //multiple choice
-      //   else if (section.sectionType === 2) {
-      //     processedSection.options = section.options
-      //     processedSection.explanation = section.explanation
-      //   }
-      //   //fill in the blank
-      //   else if (section.sectionType === 3) {
-      //     processedSection.text = section.text
-      //     processedSection.afterBlank = section.afterBlank
-      //     const rawBlank = section.blank.split(',')
-      //     processedSection.blank = rawBlank.map((str) => str.trim())
-      //     processedSection.explanation = section.explanation
-      //   }
-
-      //   processedSections.push(processedSection)
-      // }
-
-
-      // await Activity.findByIdAndUpdate(activityId, {
-      //   sections: processedSections,
-      //   latestAuthor: `${user.firstName} ${user.lastName} `,
-      // })
-
-      // const activity = await Activity.findById(activityId)
-
-      // res.status(200).json({ activity })
-
-    } catch (error) {
-      console.log(error.message)
-      res.status(400).json({ message: error.message })
+      processedPages.push(processedElements);
     }
+    await Activity.findByIdAndUpdate(activityId, {
+      pages: [...pages, (pages[pageIndex] = proccessedPages)],
+      latestAuthor: `${user.firstName} ${user.lastName} `,
+    });
+
+    const activity = await Activity.findById(activityId);
+
+    res.status(200).json({ activity });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
   }
+}
