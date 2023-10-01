c783ea9e1f72fac0864fc992c4cd6efc967e36f0
John Liu <johnliu@v1041-wn-rt-b-202-10.campus-dynamic.uwaterloo.ca>
2023-10-01 14:17:05 -0400

    index and styles

--- backups/useClickOutside.js
+++ backups/useClickOutside.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-const useClickOutside = (ref, callback) => {
-  const handleClick = (e) => {
-    if (ref.current && !ref.current.contains(e.target)) {
-      callback();
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("click", handleClick);
-    return () => {
-      document.removeEventListener("click", handleClick);
-    };
-  });
-};
+// const useClickOutside = (ref, callback) => {
+//   const handleClick = (e) => {
+//     if (ref.current && !ref.current.contains(e.target)) {
+//       callback();
+//     }
+//   };
+//   useEffect(() => {
+//     document.addEventListener("click", handleClick);
+//     return () => {
+//       document.removeEventListener("click", handleClick);
+//     };
+//   });
+// };
 
-export default useClickOutside;
+// export default useClickOutside;
