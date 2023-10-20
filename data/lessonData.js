57b00e64845a7117e0a40f6f4038382a06db9902
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-10-20 01:28:53 -0400

    total course points

---
+++ data/lessonData.js
@@ -0,0 +1,6 @@
+export const rewardData = {
+  activity: 5,
+  quiz: 10,
+  test: 20,
+  lesson: 2,
+};
--- pages/api/learn/courses.js
+++ pages/api/learn/courses.js
@@ -1,8 +1,9 @@
-import { authenticate } from "../../../utils/authenticate"
-import { verifyApiKey } from "../../../utils/verify"
-import { checkUserType } from '../../../utils/checkUserType'
-import connectDB from '../../../utils/connectDB'
-import Course from '../../../models/courseModel'
+import { authenticate } from "../../../utils/authenticate";
+import { verifyApiKey } from "../../../utils/verify";
+import { checkUserType } from "../../../utils/checkUserType";
+import connectDB from "../../../utils/connectDB";
+import Course from "../../../models/courseModel";
+import { rewardData } from "../../../data/lessonData";
 
 /**
  * @desc    Get courses for course selection
@@ -11,45 +12,75 @@ import Course from '../../../models/courseModel'
  */
 export default async function (req, res) {
   try {
-    if (req.method !== 'GET') {
-      throw new Error(`${req.method} is an invalid request method`)
+    if (req.method !== "GET") {
+      throw new Error(`${req.method} is an invalid request method`);
     }
-    verifyApiKey(req.headers.apikey)
+    verifyApiKey(req.headers.apikey);
 
     // Connect to database
-    await connectDB()
+    await connectDB();
 
     // Authenticate and get user with completed lessons, units, courses
-    const user = await authenticate(req.headers.authorization, { lessons: 1, units: 1, courses: 1 })
+    const user = await authenticate(req.headers.authorization, {
+      lessons: 1,
+      units: 1,
+      courses: 1,
+    });
 
     // Make sure user is a student
-    checkUserType(user, 1)
+    checkUserType(user, 1);
 
     const courses = await Course.find({})
-      .select('_id courseName')
+      .select("_id courseName units")
+      .populate({
+        path: "units",
+        select: "_id unitName unitNumber lessons quizzes tests",
+      });
 
-    const modifiedCourses = []
+    const modifiedCourses = [];
     // Check user for completed
     for (let i in courses) {
       modifiedCourses.push({
         _id: courses[i]._id,
         courseName: courses[i].courseName,
-        tier: 0,
-      })
+        units: courses[i].units,
+        achievedPoints: calculatedAchievedPoints(
+          courses[i].units,
+          user.completedUnits
+        ),
+        totalPoints: calculateTotalPoints(courses[i].units),
+        completed: false,
+      });
       for (let j in user.completedCourses) {
         if (
-          (user.completedCourses[j].course._id && user.completedCourses[j].course._id.equals(courses[i]._id)) ||
+          (user.completedCourses[j].course._id &&
+            user.completedCourses[j].course._id.equals(courses[i]._id)) ||
           user.completedCourses[j].course.equals(courses[i]._id)
         ) {
-          modifiedCourses[i].tier = user.completedCourses[j].tier
+          modifiedCourses[i].completed = true;
         }
       }
     }
 
-    res.json({ courses: modifiedCourses })
+    res.json({ courses: modifiedCourses });
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(400).json({ message: error.message });
   }
 }
 
+const calculateTotalPoints = (units) => {
+  let totalPoints = 0;
+  for (let i in units) {
+    totalPoints +=
+      units[i].quizzes &&
+      rewardData.quiz * units[i].quizzes.length + units[i].tests &&
+      rewardData.test * units[i].tests.length + units[i].lessons &&
+      rewardData.lesson * units[i].lessons.length;
+  }
+  return totalPoints;
+};
 
+const calculatedAchievedPoints = (units, completedUnits) => {
+  let achievedPoints = 0;
+  return achievedPoints;
+};
