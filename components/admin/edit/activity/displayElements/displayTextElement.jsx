8f2e1f9ae852d3ee6c749779d16a4ea361a19c2f
jiayishen21 <lucasshen21@gmail.com>
2023-11-01 16:41:07 -0400

    html parsing

--- components/admin/edit/activity/displayElements/displayTextElement.jsx
+++ components/admin/edit/activity/displayElements/displayTextElement.jsx
@@ -1,13 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
+import ReactHtmlParser from 'react-html-parser';
 
-export default function DisplayTextElement({element, index, theme, bold, horiz}){
-    const [data, setData] = useState(element)
+export default function DisplayTextElement({ element, index, theme, bold, horiz }) {
+    // Not sure why we did this:
+    // const [data, setData] = useState(element)
+
+    // useEffect(() => {
+    //     setData(element)
+    // }, [element])
+
+    const [parsedText, setParsedText] = useState(<Fragment></Fragment>)
 
     useEffect(() => {
-        setData(element)
+        setParsedText(ReactHtmlParser(element.text))
     }, [element])
 
-    return(
-        <div className={`text-xs ${bold ? "font-bold" : ""}`} style={{color:theme.dark}}>{element.text}</div>
+    return (
+        <div className={`text-xs ${bold ? "font-bold" : ""}`} style={{ color: theme.dark }}>{parsedText}</div>
     )
 }
