d70e31e7f470f800caf7f97d29ab558fd4ac15a9
John Liu <johnliu@v1041-wn-rt-b-173-254.campus-dynamic.uwaterloo.ca>
2023-11-03 18:56:25 -0400

    close and open properly, fix infinte render

--- components/tutorial/book/sections/backCover.jsx
+++ components/tutorial/book/sections/backCover.jsx
@@ -10,10 +10,23 @@ export default function BackCover(props) {
       }}
     >
       <div className={"w-full h-full page-front bg-transparent"}>
-        <div className={"w-full h-full bg-gray-100 p-10"}>
+        <div className={"w-full h-full bg-gray-100 p-10 flex flex-col justify-between"}
+        onClick={() => {
+          props.close()
+        }}>
           <div className="text-center text-gray-800 flex-[50%] font-galindo font-light text-xl">
-            {props.content.description}
+            <div className="flex flex-col gap-8">
+              <div className="text-2xl font-galindo">
+                Tutorial: {props.content.slidename}
+              </div>
+              {props.content.description}
+            </div>
           </div>
+          <section className="flex flex-row items-center justify-end">
+            <button className="text-xl font-galindo">
+                {/* FINSH BUTTON */}
+            </button>
+          </section>
         </div>
       </div>
     </div>
