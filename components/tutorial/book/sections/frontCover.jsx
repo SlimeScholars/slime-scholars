35aa83207f2e67648bdea6e4b131ab7c006ecf38
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-11-03 14:07:56 -0400

    book tutorial

---
+++ components/tutorial/book/sections/frontCover.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Image } from "antd";
+
+export default function FrontCover(props) {
+  return (
+    <div
+      id={"page" + props.pageNum}
+      className={`w-1/2 h-full page outer bg-transparent flip-page`}
+      style={{
+        zIndex: props.pageNum + props.totalPages - 1,
+      }}
+    >
+      <></>
+      <div className={"w-full h-full page-back bg-transparent"}>
+        <div className={"w-full h-full bg-orange-200 rounded-l-xl p-4"}>
+          <div className="flex flex-col px-4 gap-[1rem]">
+            <Image
+              src={props.content.image1}
+              preview={false}
+              className="w-auto min-h-[50%] h-[50%]"
+              placeholder={<></>}
+            />
+            <Image
+              src={props.content.image2}
+              preview={false}
+              className="w-auto min-h-[50%] h-[50%]"
+              placeholder={<></>}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
