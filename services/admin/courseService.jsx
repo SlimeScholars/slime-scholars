d09c38b29c320f379fd08c0e8c644dc1623650ea
Alex Xu <alex.xu.205@gmail.com>
2023-11-11 14:43:44 -0500

    swap course units, units created without breaks

--- services/admin/courseService.jsx
+++ services/admin/courseService.jsx
@@ -22,4 +22,26 @@ export class CourseService{
     post = async(params) => this.instance.post("/api/admin/course/create", params, applyHeaders())
         .then((response) => response)
         .catch((error) => {showToastError(error.message)});
+
+    update = async (id, newUnitsArray, unit1id, unit2id, unit1n, unit2n) => {
+        try {
+            const response = await this.instance.put(
+            "/api/admin/course/update-units",
+            {
+                courseId: id,
+                newUnitsArray,
+                unit1id,
+                unit2id,
+                unit1n,
+                unit2n,
+            },
+            applyHeaders()
+            );
+            return response;
+        } catch (error) {
+            console.log(error);
+            showToastError(error.message);
+            throw error;
+        }
+        };
 }
