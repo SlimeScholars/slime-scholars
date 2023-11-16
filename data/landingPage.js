4e7e65f92cca3e4cfdbc472e0c77eb4fc4b6596a
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-11-16 17:10:14 -0500

    fix unit test error

---
+++ data/landingPage.js
@@ -0,0 +1,28 @@
+export const landingContent = {
+  testimonials: [
+    {
+      name: "Jason P. Bell",
+      title: "Professor of Pure Mathematics @UWaterloo",
+      quote:
+        "SlimeScholars is a fun and creative new way for young people to learn mathematics. It turns learning into a game in a way that will appeal to many students and incentivize them to gain a better understanding of many of the interesting mathematical concepts that people sometimes struggle with.",
+    },
+    {
+      name: "John Doe",
+      title: "High School Calculus Teacher",
+      quote:
+        "I have been teaching calculus for 10 years and I have never seen my students so engaged in learning. SlimeScholars is a great tool for teachers to use to help their students learn and practice calculus.",
+    },
+    {
+      name: "Jane Doe",
+      title: "SAT Tutor",
+      quote:
+        "SlimeScholars is a great tool for students to use to help them prepare for the SAT. It is a fun and engaging way for students to practice their math skills.",
+    },
+    {
+      name: "Lucas Shen",
+      title: "CEO",
+      quote:
+        "ANTON MOMENT ANTON MOMENT ANTON MOMENT ANTON MOMENT ANTON MOMENT ANTON MOMENT ANTON MOMENT",
+    },
+  ],
+};
