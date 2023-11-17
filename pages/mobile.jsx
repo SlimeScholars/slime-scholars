7d63788814d5c711d7d0ec99da54016897a78831
jiayishen21 <lucasshen21@gmail.com>
2023-11-16 22:27:27 -0500

    fixed mobile navbar import

--- pages/mobile.jsx
+++ pages/mobile.jsx
@@ -1,9 +1,9 @@
 import { useRouter } from "next/router"
 import { useEffect } from "react"
 import Head from 'next/head'
-import Navbar from "../components/learn/navbar"
+import Navbar from "../components/main/navbar"
 
-export default function Mobile({ isMobile, user, setUser}) {
+export default function Mobile({ isMobile, user, setUser }) {
   const router = useRouter()
 
   useEffect(() => {
@@ -22,28 +22,28 @@ export default function Mobile({ isMobile, user, setUser}) {
   return (
     <>
       <div className="min-w-screen flex flex-col overflow-hidden">
-      <Head>
-        <title>Slime Scholars</title>
-        <meta name="description" content="Make Learning Exiting with Slime Scholars" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="w-screen flex flex-col items-center justify-center flex-1">
-        <Navbar setUser={setUser} user={user}/>
-        <header className="min-w-screen min-h-screen flex flex-col items-center justify-center content-center">
-          <div className="mr-[3rem] ml-[3rem] overflow-hidden">
-            <h1 className="font-black text-5xl max-w-4xl text-center ">
-              Make Learning Exciting with Slime Scholars
-            </h1>
-            <h2 className="font-light text-3xl my-6 text-center max-w-4xl leading-relaxed text-gray-600">
-              Slime Scholars is a project that motivates students to learn by making the process gratifying.
-            </h2>
-            <h2 className="font-galindo text-4xl text-center">
-              Coming to <span className="text-pink-500">appstore</span> soon!
-            </h2>
-          </div>
-        </header>
-      </main>
-    </div>
+        <Head>
+          <title>Slime Scholars</title>
+          <meta name="description" content="Make Learning Exiting with Slime Scholars" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="w-screen flex flex-col items-center justify-center flex-1">
+          <Navbar setUser={setUser} user={user} />
+          <header className="min-w-screen min-h-screen flex flex-col items-center justify-center content-center">
+            <div className="mr-[3rem] ml-[3rem] overflow-hidden">
+              <h1 className="font-black text-5xl max-w-4xl text-center ">
+                Make Learning Exciting with Slime Scholars
+              </h1>
+              <h2 className="font-light text-3xl my-6 text-center max-w-4xl leading-relaxed text-gray-600">
+                Slime Scholars is a project that motivates students to learn by making the process gratifying.
+              </h2>
+              <h2 className="font-galindo text-4xl text-center">
+                Coming to <span className="text-pink-500">App Store</span> soon!
+              </h2>
+            </div>
+          </header>
+        </main>
+      </div>
     </>
   )
 }
