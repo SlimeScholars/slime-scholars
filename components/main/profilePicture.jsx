d56ccd9ffa7fea4ecf30893e4affb2ddbca1a1d9
John Liu <johnliu@Johns-MacBook-Air.local>
2023-11-18 20:37:16 -0500

    friends pfps

--- components/main/profilePicture.jsx
+++ components/main/profilePicture.jsx
@@ -31,7 +31,56 @@ export default function ProfilePicture({user}){
     </div>
     : 
     // Handle the case when user or user.pfpSlime is null or undefined
-    <div className="default-image">
-        {"/assets/pfp/slimes/blue-slime.png"}
+    <Image
+    src={
+        "/assets/pfp/slimes/red-slime.png"
+    }
+    alt={"null pfp"}
+    height={0}
+    width={0}
+    sizes="100vw"
+    className="relative z-10 translate-y-1/3 scale-150 w-[3.5rem] h-[3.5rem] max-xl:w-[3rem] max-xl:h-[2.5rem] max-xl:scale-350 max-xl:translate-y-1/2"
+    />
+}
+
+export function SmallProfilePicture({user}){
+    return user && user.pfpSlime ? 
+    <div className="relative flex items-center justify-center">
+        <div className="absolute h-32 w-32 overflow-hidden">
+        <Image
+            src={
+            "/assets/pfp/backgrounds/" +
+            gameData.items[user.pfpBg].bg
+            }
+            alt={user.pfpBg}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="absolute h-32 w-32"
+        />
+        </div>
+        <Image
+        src={
+            "/assets/pfp/slimes/" +
+            gameData.slimes[user.pfpSlime].pfp
+        }
+        alt={user.pfpSlime}
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="relative z-10 translate-y-1/3 scale-150 w-[3.5rem] h-[3.5rem] max-xl:w-[3rem] max-xl:h-[2.5rem] max-xl:scale-350 max-xl:translate-y-1/2"
+        />
     </div>
+    : 
+    // Handle the case when user or user.pfpSlime is null or undefined
+    <Image
+    src={
+        "/assets/pfp/slimes/red-slime.png"
+    }
+    alt={"null pfp"}
+    height={0}
+    width={0}
+    sizes="100vw"
+    className="relative z-10 translate-y-1/3 scale-150 w-[3.5rem] h-[3.5rem] max-xl:w-[3rem] max-xl:h-[2.5rem] max-xl:scale-350 max-xl:translate-y-1/2"
+    />
 }
--- components/play/friends/leadboardListing.jsx
+++ components/play/friends/leadboardListing.jsx
@@ -1,48 +1,24 @@
 import Image from "next/image.js";
 import { gameData } from "../../../data/gameData.js";
+import { SmallProfilePicture} from "../../main/profilePicture.jsx";
 
 export default function LeadboardListing({ user, userRank, users, userId, colorPalette }) {
   return (
     <div id="friends-listing">
       {Array.isArray(users) ? (
-        users.map((user, index) => {
-          const pfpBg = user.pfpBg || ''; // Ensure pfpBg has a default value
-          const pfpSlime = user.pfpSlime || ''; // Ensure pfpSlime has a default value
-          if (user._id === userId) {
+        users.map((opt_user, index) => {
+          if (opt_user._id === userId) {
             return (
               <div
-                className="flex flex-row items-center py-4 "
+                className="flex flex-row items-center py-1"
                 style={{
                   background: colorPalette ? colorPalette.primary1 : "none",
                 }}
                 key={index}
               >
                 <div className="grow-0 px-4">{userRank === undefined ? index + 1 : userRank}</div>
-                <div className="w-10 h-10 rounded-full overflow-hidden">
-                  <div className="relative">
-                    <Image
-                      src={
-                        "/assets/pfp/backgrounds/" +
-                        gameData.items[pfpBg].pfp
-                      }
-                      alt={user.pfpBg}
-                      height={0}
-                      width={0}
-                      sizes='100vw'
-                      className="absolute inset-0 w-full h-full"
-                    />
-                    <Image
-                      src={
-                        "/assets/pfp/slimes/" +
-                        gameData.slimes[pfpSlime].pfp
-                      }
-                      alt={user.pfpSlime}
-                      height={0}
-                      width={0}
-                      sizes='100vw'
-                      className="relative z-10 translate-y-1/4 scale-125 h-10 w-10"
-                    />
-                  </div>
+                <div className="w-16 h-16 rounded-full overflow-hidden scale-75 ">
+                  <SmallProfilePicture user={user}/>
                 </div>
                 <div className="grow px-4">{user.username}</div>
                 <div className="grow-0 pr-4">{user.exp} exp</div>
@@ -50,36 +26,13 @@ export default function LeadboardListing({ user, userRank, users, userId, colorP
             );
           } else {
             return (
-              <div className="flex flex-row items-center py-4" key={index}>
+              <div className="flex flex-row items-center py-1" key={index}>
                 <div className="grow-0 px-4">{index + 1}</div>
-                <div className="w-10 h-10 rounded-full overflow-hidden">
-                  <div className="relative">
-                    <Image
-                      src={
-                        "/assets/pfp/backgrounds/" +
-                        gameData.items[user.pfpBg].bg
-                      }
-                      alt={user.pfpBg}
-                      height={0}
-                      width={0}
-                      sizes='100vw'
-                      className="absolute inset-0 w-full h-full"
-                    />
-                    <Image
-                      src={
-                        "/assets/pfp/slimes/" +
-                        gameData.slimes[user.pfpSlime].pfp
-                      }
-                      alt={user.pfpSlime}
-                      height={0}
-                      width={0}
-                      sizes='100vw'
-                      className="relative z-10 translate-y-1/4 scale-125 h-10 w-10"
-                    />
-                  </div>
+                <div className="w-16 h-16 rounded-full overflow-hidden scale-75">
+                    <SmallProfilePicture user={opt_user}/>
                 </div>
-                <div className="grow px-4">{user.username}</div>
-                <div className="grow-0 pr-4">{user.exp} exp</div>
+                <div className="grow px-4">{opt_user.username}</div>
+                <div className="grow-0 pr-4">{opt_user.exp} exp</div>
               </div>
             );
           }
