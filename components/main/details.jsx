9952c3733e82407c307f98057d5b732c1a50033c
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-12-06 23:18:10 -0500

    responsive details and banner

--- components/main/details.jsx
+++ components/main/details.jsx
@@ -7,7 +7,7 @@ export default function Details() {
   return (
     <section
       id="details"
-      className="w-screen py-32 bg-bg-light relative flex flex-col items-center justify-center px-80"
+      className="w-screen py-32 bg-bg-light relative flex flex-col items-center justify-center px-8 sm:px-16 md:px-24 lg:px-48 xl:px-80"
       style={{
         backgroundImage: "url('/assets/graphics/bg-graphic-2.svg')",
         backgroundSize: "cover",
@@ -26,9 +26,9 @@ export default function Details() {
       <div className="w-full h-[300px]" data-aos="zoom-in-up">
         <Preview />
       </div>
-      <div className="w-full grid grid-cols-2 gap-10 z-10">
+      <div className="w-full flex flex-col sm:grid sm:grid-cols-2 sm:gap-10 z-10">
         <div
-          className="relative w-full h-full ring-8 ring-primary bg-white mt-10"
+          className="relative w-full h-fit pb-5 ring-8 ring-primary bg-white mt-10"
           data-aos="zoom-in-left"
           data-aos-delay="100"
         >
@@ -39,15 +39,17 @@ export default function Details() {
             alt="Octopus Slime"
             className="absolute w-32 h-32 -top-20 -left-16 -rotate-[27deg] z-20"
           />
+          <Image
+            className="absolute top-0 left-0 saturate-50 contrast-50 opacity-40 filter z-0 object-cover object-top"
+            src="/assets/graphics/parents.png"
+            alt="Parents.png"
+            fill={true}
+          />
           <div className="relative w-full h-full flex flex-col items-center justify-start overflow-hidden pt-8 px-10">
-            <Image
-              className="absolute top-0 left-0 saturate-50 contrast-50 opacity-40 filter z-0 object-cover object-top"
-              src="/assets/graphics/parents.png"
-              alt="Parents.png"
-              fill={true}
-            />
-            <h3 className="text-3xl text-primary font-galindo z-20">Parents</h3>
-            <ul className="w-full pl-5 mt-4 font-normal text-xl text-green-800 !list-inside !list-desc space-y-5 z-20">
+            <h3 className="text-2xl md:text-3xl text-primary font-galindo z-20">
+              Parents
+            </h3>
+            <ul className="w-full pl-5 mt-4 font-normal text-lg md:text-xl text-green-800 styled-list space-y-3 lg:space-y-5 z-20">
               <li>Monitor your child's progress</li>
               <li>Get insights on your child's learning</li>
               <li>Help your child learn at home</li>
@@ -59,14 +61,14 @@ export default function Details() {
               <svg>
                 <rect x="0" y="0" fill="none" width="100%" height="100%" />
               </svg>
-              <p className="text-white font-extrabold text-lg">
+              <p className="text-white font-extrabold text-base md:text-lg">
                 SlimeScholars Parents
               </p>
             </Link>
           </div>
         </div>
         <div
-          className="relative w-full h-full ring-8 ring-primary bg-white mt-10"
+          className="relative w-full h-fit pb-5 ring-8 ring-primary bg-white mt-10"
           data-aos="zoom-in-right"
           data-aos-delay="200"
         >
@@ -77,17 +79,17 @@ export default function Details() {
             alt="Octopus Slime"
             className="absolute w-40 h-40 -bottom-10 -right-16 rotate-[9deg] z-20"
           />
+          <Image
+            className="absolute top-0 left-0 saturate-50 contrast-50 opacity-40 filter z-0 object-cover"
+            src="/assets/graphics/teachers.png"
+            alt="Teachers.png"
+            fill={true}
+          />
           <div className="relative w-full h-full flex flex-col items-center justify-start z-10 overflow-hidden pt-8 px-10">
-            <Image
-              className="absolute top-0 left-0 saturate-50 contrast-50 opacity-40 filter z-0 object-cover"
-              src="/assets/graphics/teachers.png"
-              alt="Teachers.png"
-              fill={true}
-            />
-            <h3 className="text-3xl text-primary font-galindo z-20">
+            <h3 className="text-2xl md:text-3xl text-primary font-galindo z-20">
               Teachers
             </h3>
-            <ul className="w-full pl-5 mt-4 font-normal text-xl text-green-800 !list-desc space-y-5 z-20">
+            <ul className="w-full pl-5 mt-4 font-normal text-lg md:text-xl text-green-800 styled-list space-y-3 lg:space-y-5 z-20">
               <li>Create engaging lessons</li>
               <li>Reward your students' learning</li>
               <li>Manage assignments and homework</li>
@@ -99,7 +101,7 @@ export default function Details() {
               <svg>
                 <rect x="0" y="0" fill="none" width="100%" height="100%" />
               </svg>
-              <p className="text-white font-extrabold text-lg">
+              <p className="text-white font-extrabold text-base md:text-lg">
                 SlimeScholars Teachers
               </p>
             </Link>
