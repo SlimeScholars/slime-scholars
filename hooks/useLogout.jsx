90a13efa15cb126d844c63209233bbd5de448827
jazyz <johnlikeapple37@gmail.com>
2023-12-21 16:23:50 -0500

    refresh website after logging out

--- hooks/useLogout.jsx
+++ hooks/useLogout.jsx
@@ -3,6 +3,7 @@ import cookies from "../services/cookies/cookies";
 export default function useLogout(){
     const logout = () => {
         cookies.remove("slime-scholars-webapp-token")
+        window.location.reload();
     }
     return {logout}
 }
