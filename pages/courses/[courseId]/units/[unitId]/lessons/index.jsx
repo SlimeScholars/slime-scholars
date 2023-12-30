139b021b72dff4c37f15096e47097476a127f646
jiayishen21 <lucasshen21@gmail.com>
2023-12-30 18:48:21 -0500

    activity completion status

--- pages/courses/[courseId]/units/[unitId]/lessons/index.jsx
+++ pages/courses/[courseId]/units/[unitId]/lessons/index.jsx
@@ -118,87 +118,87 @@ export default function Lessons({ user, loading, setLoading, colorPalette }) {
         backgroundColor: !colorPalette ? "" : colorPalette.primary2 + "50",
       }}
     >
-      {lessons.length == 0 ? 
-      <div className="relative z-[1] w-full h-[calc(100vh_-_5rem)]">
-        <MainSpinner bgStyle={{backgroundColor: "#000000A0", color: "white"}} text="Fetching Lessons"/>
-      </div>
-      :
-      <>
-      <div className="h-[2.5rem] bg-white flex flex-row gap-3 items-center pl-[4rem]">
-        <span
-          className="hover:text-blue-400 transition-all duration-150"
-          onClick={() => {
-            router.push(`/courses`);
-          }}
-        >
-          Courses{" "}
-        </span>
-        <span>{">>"}</span>
-        <span
-          className="hover:text-blue-400 transition-all duration-150"
-          onClick={() => {
-            router.push(`/courses/${courseId}/units`);
-          }}
-        >
-          {courseName}
-        </span>
-        <span>{">>"}</span>
-        <span
-          className="hover:text-blue-400 transition-all duration-150"
-          onClick={() => {
-            router.push(`/courses/${courseId}/units/${unitId}/lessons`);
-          }}
-        >
-          {unitName}
-        </span>
-      </div>
-      <div
-        className="relative z-[1] w-full h-[calc(100vh_-_5rem_-_2.5rem)] flex flex-col pl-[3rem] 2xl:pl-[3.75rem] pr-[1rem] pb-[15rem] overflow-y-scroll"
-        style={{
-          backgroundColor: !colorPalette ? "" : colorPalette.black + "C0",
-        }}
-      >
-        <div className="pt-[1.5rem] pb-[1.5rem]">
-          <section
-            className="text-4xl font-bold flex flex-col justify-center h-full"
+      {lessons.length == 0 ?
+        <div className="relative z-[1] w-full h-[calc(100vh_-_5rem)]">
+          <MainSpinner bgStyle={{ backgroundColor: "#000000A0", color: "white" }} text="Fetching Lessons" />
+        </div>
+        :
+        <>
+          <div className="h-[2.5rem] bg-white flex flex-row gap-3 items-center pl-[4rem]">
+            <span
+              className="hover:text-blue-400 transition-all duration-150"
+              onClick={() => {
+                router.push(`/courses`);
+              }}
+            >
+              Courses{" "}
+            </span>
+            <span>{">>"}</span>
+            <span
+              className="hover:text-blue-400 transition-all duration-150"
+              onClick={() => {
+                router.push(`/courses/${courseId}/units`);
+              }}
+            >
+              {courseName}
+            </span>
+            <span>{">>"}</span>
+            <span
+              className="hover:text-blue-400 transition-all duration-150"
+              onClick={() => {
+                router.push(`/courses/${courseId}/units/${unitId}/lessons`);
+              }}
+            >
+              {unitName}
+            </span>
+          </div>
+          <div
+            className="relative z-[1] w-full h-[calc(100vh_-_5rem_-_2.5rem)] flex flex-col pl-[3rem] 2xl:pl-[3.75rem] pr-[1rem] pb-[15rem] overflow-y-scroll"
             style={{
-              color: !colorPalette ? "" : colorPalette.white,
+              backgroundColor: !colorPalette ? "" : colorPalette.black + "C0",
             }}
           >
-            {unitName}
-            <div className="flex flex-row">
-              <span
-                className="text-lg italic font-normal"
+            <div className="pt-[1.5rem] pb-[1.5rem]">
+              <section
+                className="text-4xl font-bold flex flex-col justify-center h-full"
                 style={{
-                  color: !colorPalette ? "" : colorPalette.text1 + "C0",
+                  color: !colorPalette ? "" : colorPalette.white,
                 }}
               >
-                {counts.lessons} lessons • {counts.quizzes} quizzes •{" "}
-                {counts.tests} tests • {sumPoints(lessons)}
-              </span>
-              <Image
-                src="/assets/icons/flower.png"
-                alt="flowers"
-                height={0}
-                width={0}
-                sizes="100vw"
-                className="h-[1.4rem] w-[1.4rem] mx-1 brightnes"
-              />
+                {unitName}
+                <div className="flex flex-row">
+                  <span
+                    className="text-lg italic font-normal"
+                    style={{
+                      color: !colorPalette ? "" : colorPalette.text1 + "C0",
+                    }}
+                  >
+                    {counts.lessons} lessons • {counts.quizzes} quizzes •{" "}
+                    {counts.tests} tests • {sumPoints(lessons)}
+                  </span>
+                  <Image
+                    src="/assets/icons/flower.png"
+                    alt="flowers"
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="h-[1.4rem] w-[1.4rem] mx-1 brightnes"
+                  />
+                </div>
+              </section>
             </div>
-          </section>
-        </div>
-        <div className="flex flex-col">
-          {lessons.map((lesson) => (
-            <Lesson
-              key={lesson._id}
-              courseId={courseId}
-              unitId={unitId}
-              lesson={lesson}
-              colorPalette={colorPalette}
-            />
-          ))}
-        </div>
-      </div></>}
+            <div className="flex flex-col">
+              {lessons.map((lesson) => (
+                <Lesson
+                  key={lesson._id}
+                  courseId={courseId}
+                  unitId={unitId}
+                  lesson={lesson}
+                  colorPalette={colorPalette}
+                />
+              ))}
+            </div>
+          </div></>}
     </div>
   );
 }
